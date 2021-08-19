29aaed846f6b4c63fffb25d5d97a144749d2e4d5
rhuan <rhuan.marknet@gmail.com>
2021-08-19 02:01:45 -0300

    refresh

--- dont_touch/main2.sh
+++ dont_touch/main2.sh
@@ -48,7 +48,7 @@ echo "======================CHROME======================"
 echo
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb ;
-dpkg -i google-chrome-stable_current_amd64.deb ;
+dpkg -i ./google-chrome-stable_current_amd64.deb ;
 
 echo
 echo "======================SNAP======================"
