86218152952098eb23cc1d09bd09d7fbf1e986c9
rhuan <rhuan.marknet@gmail.com>
2021-08-19 02:22:41 -0300

    refresh

--- dont_touch/main2.sh
+++ dont_touch/main2.sh
@@ -77,6 +77,15 @@ echo
 apt install filezilla -y ;
 
 echo
+echo "======================ANYDESK======================"
+echo
+
+wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add - ;
+echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list ;
+apt update -y ;
+apt install anydesk -y ;
+
+echo
 echo "======================LIMPAR======================"
 echo
 
