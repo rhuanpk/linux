ecb394a0b1b4d8b893dc63471f5de4af2faa2f9a
rhuan <rhuan.pksf@gmail.com>
2021-09-19 09:08:49 -0300

    refresh!

--- standard_scripts/script-pasta.sh
+++ standard_scripts/script-pasta.sh
@@ -11,3 +11,6 @@ sudo cp -v ./*.sh /scripts
 
 echo -e "" >> "$HOME"/.bashrc
 echo 'export PATH=$PATH:/scripts' >> "$HOME"/.bashrc
+
+sudo echo -e "" >> /root/.bashrc
+sudo echo 'export PATH=$PATH:/scripts' >> /root/.bashrc
