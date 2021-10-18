5ac0f12de4030f1363b5fc8ba103f2fc7dd1a033
Rhuan Patriky <84540130+rhuan-pk@users.noreply.github.com>
2021-10-18 18:00:36 -0300

    Update backup.sh

--- standard_scripts/backup.sh
+++ standard_scripts/backup.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-arquivo="$(date +"%d-%m-%y_backup").tar.gz"
-busca=("/tmp/temp1/" "/tmp/temp2/" "/tmp/temp3/")
-main="${HOME}/backup"
-destino="/tmp/temp"
+arquivo="$(date +"%d-%m-%y")_backup.tar.gz"
+busca=("${HOME}/Desktop" "${HOME}/Documents" "${HOME}/Downloads" "${HOME}/Pictures" "${HOME}/Videos")
+main="${HOME}/loja_suporte"
+destino="${HOME}/Dropbox/backup"
 
+rm -rf ${main}/*
 rm -rf ${destino}/*.tar.gz
 
 for (( i = 0; i < ${#busca[@]}; ++i )); do
-	cp -r ${busca[i]} ${main}
+        cp -r ${busca[i]} ${main}
 done
 
 tar -zcvf ${destino}/${arquivo} ${main}
