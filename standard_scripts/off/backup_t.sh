de0329b552e402bd4620633603c91855916f1d0e
rhuan-pk <rhuan.pksf@gmail.com>
2021-10-19 10:59:48 -0300

    add off/ em standard

--- standard_scripts/off/backup_t.sh
+++ standard_scripts/off/backup_t.sh
@@ -8,11 +8,11 @@
 
 data="$(date +"%d-%m-%y")"
 arquivo="${data}_backup.tar.gz"
-busca=("${HOME}/Documentos" "${HOME}/Downloads" "${HOME}/Imagens" "${HOME}/Vídeos")
-main="${HOME}/backup"
+busca=("${HOME}/Desktop" "${HOME}/Documents" "${HOME}/Downloads" "${HOME}/Pictures" "${HOME}/Videos")
+main="${HOME}/loja_suporte"
 destino="${HOME}/Cloud/backup"
 montagem="/root/mnt/back-up"
-arquivo_log="/var/log/backup_diario.log"
+arquivo_log="${HOME}/.backup_diario.log"
 
 # Variáveis para guardar parte das mensagens de log
 
@@ -21,7 +21,7 @@ msg_ext="processo EXTERNO"
 
 # Variável para guardar a senha do sudo
 
-password="12345"
+password="marknet"
 
 ########################################################
 # Sessão de declaração de funções
@@ -45,15 +45,17 @@ for (( i = 0; i < ${#busca[@]}; ++i )); do
         cp -rv ${busca[i]} ${main}
 done
 
+echo "--- pk ---" >> ${arquivo_log}
+
 # --- Backup para armazenamento em Cloud ---
 
 # Remoção do backup antigo ---> (PROCESSO EXCLUSÃO)
 
 if rm -rfv ${destino}/*.tar.gz 2> /dev/null; then
-	echo -e "[${data}] - Backup antigo excluido! (${msg_cloud} EXCLUSÃO) - SUCESSO" >> ${arquivo_log}
+	echo -e "[${data}] - Backup antigo excluido! (${msg_cloud} exclusão) - SUCESSO" >> ${arquivo_log}
 	excluido_nuvem=0
 else
-	echo -e "[${data}] - Backup antigo não excluido! (${msg_cloud} EXCLUSÃO) - FALHA" >> ${arquivo_log}
+	echo -e "[${data}] - Backup antigo não excluido! (${msg_cloud} exclusão) - FALHA" >> ${arquivo_log}
 	excluido_nuvem=1
 fi	
 
@@ -61,9 +63,9 @@ fi
 
 if [ ${excluido_nuvem} -eq 0 ]; then
 	if tar -zcvf ${destino}/${arquivo} ${main} 2> /dev/null; then
-		echo -e "[${data}] - Novo backup criado! (${msg_cloud} ADIÇÃO) - SUCESSO" >> ${arquivo_log}
+		echo -e "[${data}] - Novo backup criado! (${msg_cloud} adição) - SUCESSO" >> ${arquivo_log}
 	else
-		echo -e "[${data}] - Novo backup não criado! (${msg_cloud} ADIÇÃO) - FALHA" >> ${arquivo_log}
+		echo -e "[${data}] - Novo backup não criado! (${msg_cloud} adição) - FALHA" >> ${arquivo_log}
 	fi
 fi
 
@@ -72,16 +74,17 @@ fi
 # Verifica se o exite o ponto para montagem do disco externo, caso não existir, seja criado ---> (PROCESSO 1)
 
 if [ ! -d ${montagem} ]; then
-	auto_sudo "mkdir -pv ${montagem}"
-	if [ ! -d ${montagem} ]; then
+	echo -e "[${data}] - Diretorio de montagem já existente (${msg_ext} 1) - SUCESSO" >> ${arquivo_log}
+	cria_ponto=0
+else
+	echo -e "${password}\n" | sudo -S su -c "mkdir -v ${montagem}"
+	if [ -d ${montagem} ]; then
+		echo -e "[${data}] - Diretorio de montagem criado (${msg_ext} 1) - SUCESSO" >> ${arquivo_log}
+		cria_ponto=0
+	else
 		echo -e "[${data}] - Diretorio de montagem não criado (${msg_ext} 1) - FALHA" >> ${arquivo_log}
 		cria_ponto=1
 	fi
-	echo -e "[${data}] - Diretorio de montagem criado (${msg_ext} 1) - SUCESSO" >> ${arquivo_log}
-	cria_ponto=0
-else
-	echo -e "[${data}] - Diretorio de montagem já existente (${msg_ext} 1) - SUCESSO" >> ${arquivo_log}
-	cria_ponto=0
 fi
 
 # Verificar se o disco externo está montado, se não estiver, tenta montar ---> (PROCESSO 2)
@@ -104,7 +107,7 @@ fi
 
 # Backup para o disco externo ---> (PROCESSO 3)
 
-if [ ${montado} -eq 0 ]; then
+if [ ${montado} -eq 0 ] 2> /dev/null; then
 
 	# Remoção do backup antigo no disco externo
 
