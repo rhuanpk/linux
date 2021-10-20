3e0f6b8a41901ad02233c6e04f7de3a268ba16a6
rhuan-pk <rhuan.pksf@gmail.com>
2021-10-20 04:25:46 -0300

    finalizado backup simples

--- standard_scripts/backup.sh
+++ standard_scripts/backup.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
+###########################################################################
+#
+# Script criado para fazer backup soimples
+# para alguma pasta de serviço de cloud e
+# para algum disco externo
+#
+# Modo de uso:
+#
+#	busca - pastas que seram feitas o backup
+#	espaco - nome para diferencias arquivos de backup
+#	main - pasta aonde irá as pastas a serem feitas o backup
+#	destino - pasta do serivo de cloud storage
+#	externo - caminho do disco externo
+#		OBS: MONTAR ATOMÁTICAMENTE O DISCO EXTERNO (para ficar
+#		     permissões de usuário normal)
+#
+##########################################################################
+
 busca=("${HOME}/Desktop" "${HOME}/Documents" "${HOME}/Downloads" "${HOME}/Pictures" "${HOME}/Videos")
 espaco="home"
 arquivo="$(date +"%d-%m-%y")_${espaco}_backup.tar.gz"
