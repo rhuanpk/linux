d6fdfc856b495ded81ac557d796f24553b9b4b30
rhuan-pk <rhuan.pksf@gmail.com>
2021-10-20 04:18:07 -0300

    finalizado backup simples

--- standard_scripts/backup.sh
+++ standard_scripts/backup.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
-arquivo="$(date +"%d-%m-%y")_backup.tar.gz"
 busca=("${HOME}/Desktop" "${HOME}/Documents" "${HOME}/Downloads" "${HOME}/Pictures" "${HOME}/Videos")
+espaco="home"
+arquivo="$(date +"%d-%m-%y")_${espaco}_backup.tar.gz"
 main="${HOME}/backup"
-destino="${HOME}/Dropbox/backup"
+destino="${HOME}/cloud/backup"
+externo="/media/${USER}/99B0-A454"
 
 rm -rfv ${main}/*
 rm -rfv ${destino}/*.tar.gz
+rm -rfv ${externo}/*${espaco}*
 
 for (( i = 0; i < ${#busca[@]}; ++i )); do
         cp -rv ${busca[i]} ${main}
 done
 
 tar -zcvf ${destino}/${arquivo} ${main}
+tar -zcvf ${externo}/${arquivo} ${main}
