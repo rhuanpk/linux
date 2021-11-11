d9e7c676eaaf040e86b9983d4ae5acd62cdf5d49
rhuan-cork <rhuan.cork@gamil.com>
2021-11-11 20:03:23 -0300

    refresh!

--- standard_scripts/contador.sh
+++ standard_scripts/contador.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 read foo
+[ "${foo,,}" == "q" ] && exit 0
 bar=${foo}
 clear
 echo -ne "  >>> CONTADOR = ${bar} !\r"
 
 while : ; do
 	read foo 
+	[ "${foo,,}" == "q" ] && exit 0
 	let bar+=${foo}
 	clear
 	echo -ne "  >>> CONTADOR = ${bar} !\r"
