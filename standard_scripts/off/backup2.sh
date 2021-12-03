3a3fd87f29562cf94d79efd15f7eed3c79e1a449
rhuan-pk <rhuan.pksf@gmail.com>
2021-12-03 13:55:47 -0300

    refresh!

--- standard_scripts/off/backup2.sh
+++ standard_scripts/off/backup2.sh
@@ -39,12 +39,16 @@ log_file="/tmp/tmp/backup_teste/backup_file.log"
 
 echo -e "\n    ~     ~     ~" >> "${log_file}"
 
+get_stt() {
+	stt=$(zsh -c 'for dir in /tmp/tmp/backup_teste/backup/*; do rm -rf ${dir}; done; echo "${?}"')
+	echo "${stt}"
+}
+
 for ((i=0;i<=1;++i)); do
 	if [ ${i} -eq 0 ]; then
 		echo "here remocao"
-		aux=$(for foo in ${main}/*; do rm -rfv ${foo}; done 2>&1)
-		echo "${?}"
-		if [ ${?} -ne 0 ]; then
+		echo "----- retorno da função: $(get_stt) --------"
+		if [ $(get_stt) -ne 0 ]; then
 			echo "if falha remocao"
 			echo -e "\n[${data} * $(date +%T)] --- BACKUP NÃO INICIADO ---\n" >> "${log_file}"
 			echo -e "[${data} * $(date +%T)] STDERR (remoção): ${aux}" >> "${log_file}"
