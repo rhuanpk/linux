05b05a6bbb68b8753fa78ecbec5f4fa763425d20
rhuan-pk <rhuan.pksf@gmail.com>
2021-12-29 04:31:45 -0300

    push.sh: rodado seco copia todos os arquivos, pode passar por parâmetros sem proteção alguns arquivos específicos para serem copiados somente eles

--- standard_scripts/mover.sh
+++ standard_scripts/mover.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# Move os scripts para a pasta correta;
-# Correr o script dentro pasta
+# Move os scripts para a pasta correta
 
-sudo cp -v ./*.sh /usr/local/bin/
+[ "${*}" = "" ] && sudo cp -v ./*.sh /usr/local/bin/ || for i in ${@}; do sudo cp -v ./${i} /usr/local/bin/; done
