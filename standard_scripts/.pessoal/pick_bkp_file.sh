63b74513ab38fac07b2550bbf9b6e448fa1d0196
Rhuan Patriky <rhuan.pksf@gmail.com>
2022-01-05 14:56:42 -0300

    feito limpa: excluido alguns desnecessários, alterado o nome de alguns e adicionado o cmr.sh e o pick_bkp_file.sh

---
+++ standard_scripts/.pessoal/pick_bkp_file.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+WAY_BKP="/home/${USER}/Documents/config_files_backup"
+WAY_OPT="/opt"
+WAY_FONTES="/home/${USER}/Documents/fonts"
+WAY_ICONES="/home/${USER}/.icons"
+WAY_TEMAS="/home/${USER}/.themes"
+
+ls -1 ${WAY_OPT} | cat -n | tr -s ' ' > ${WAY_BKP}/opts/programas_opt.txt
+ls -1 ${WAY_FONTES} | cat -n | tr -s ' ' > ${WAY_BKP}/fontes/lista_fontes.txt
+ls -1 ${WAY_ICONES} | cat -n | tr -s ' ' > ${WAY_BKP}/icones/lista_icones.txt
+ls -1 ${WAY_TEMAS} | cat -n | tr -s ' ' > ${WAY_BKP}/temas/lista_temas.txt
--- standard_scripts/primeira_atualizacao.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Para quando ainda não tiver sido feito nenhuma atualização no sistema
-
-apt update -y
-apt upgrade -y
-
-ubuntu-drivers autoinstall
-apt install ubuntu-restricted-extras -y
