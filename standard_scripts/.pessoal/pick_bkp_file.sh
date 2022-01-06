b57e2fbb0cb17f57994277e77222c880d3b0cdce
Rhuan Patriky <rhuan.pksf@gmail.com>
2022-01-06 15:08:19 -0300

    pick_bkp_file.sh: adicionado arquivos de configuração do vscode

--- standard_scripts/.pessoal/pick_bkp_file.sh
+++ standard_scripts/.pessoal/pick_bkp_file.sh
@@ -5,8 +5,10 @@ WAY_OPT="/opt"
 WAY_FONTES="/home/${USER}/Documents/fonts"
 WAY_ICONES="/home/${USER}/.icons"
 WAY_TEMAS="/home/${USER}/.themes"
+WAY_VSCODE="/home/${USER}/.config/Code/User"
 
 ls -1 ${WAY_OPT} | cat -n | tr -s ' ' > ${WAY_BKP}/opts/programas_opt.txt
 ls -1 ${WAY_FONTES} | cat -n | tr -s ' ' > ${WAY_BKP}/fontes/lista_fontes.txt
 ls -1 ${WAY_ICONES} | cat -n | tr -s ' ' > ${WAY_BKP}/icones/lista_icones.txt
 ls -1 ${WAY_TEMAS} | cat -n | tr -s ' ' > ${WAY_BKP}/temas/lista_temas.txt
+cp -ru ${WAY_VSCODE}/* ${WAY_BKP}/vscode
