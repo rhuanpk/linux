b1dc80bd527e5d82b940ecbcaea766e0de048828
Rhuan Patriky <rhuan.pksf@gmail.com>
2022-01-06 15:17:00 -0300

    pick_bkp_file.sh: atualizado variáveis vscode e adicionado extensões do vscode para backup também

--- standard_scripts/.pessoal/pick_bkp_file.sh
+++ standard_scripts/.pessoal/pick_bkp_file.sh
@@ -5,10 +5,12 @@ WAY_OPT="/opt"
 WAY_FONTES="/home/${USER}/Documents/fonts"
 WAY_ICONES="/home/${USER}/.icons"
 WAY_TEMAS="/home/${USER}/.themes"
-WAY_VSCODE="/home/${USER}/.config/Code/User"
+WAY_VSCODECFG="/home/${USER}/.config/Code/User"
+WAY_VSCODEEXT="/home/${USER}/.vscode/extensions"
 
 ls -1 ${WAY_OPT} | cat -n | tr -s ' ' > ${WAY_BKP}/opts/programas_opt.txt
 ls -1 ${WAY_FONTES} | cat -n | tr -s ' ' > ${WAY_BKP}/fontes/lista_fontes.txt
 ls -1 ${WAY_ICONES} | cat -n | tr -s ' ' > ${WAY_BKP}/icones/lista_icones.txt
 ls -1 ${WAY_TEMAS} | cat -n | tr -s ' ' > ${WAY_BKP}/temas/lista_temas.txt
-cp -ru ${WAY_VSCODE}/* ${WAY_BKP}/vscode
+cp -ru ${WAY_VSCODECFG}/* ${WAY_BKP}/vscode/configuracoes
+cp -ru ${WAY_VSCODEEXT}/* ${WAY_BKP}/vscode/extensoes
