8ac671efe3f400f89553481b1e41dff002262971
Rhuan Patriky <rhuan.pksf@gmail.com>
2022-01-12 17:55:44 -0300

    pick_bkp_file.sh: adicionado caminho para o cfg.yaml do "noti"

--- standard_scripts/.pessoal/pick_bkp_file.sh
+++ standard_scripts/.pessoal/pick_bkp_file.sh
@@ -7,10 +7,12 @@ WAY_ICONES="/home/${USER}/.icons"
 WAY_TEMAS="/home/${USER}/.themes"
 WAY_VSCODECFG="/home/${USER}/.config/Code/User"
 WAY_VSCODEEXT="/home/${USER}/.vscode/extensions"
+WAY_NOTI="/home/marknet06/.config/noti"
 
 ls -1 ${WAY_OPT} | cat -n | tr -s ' ' > ${WAY_BKP}/opts/programas_opt.txt
 ls -1 ${WAY_FONTES} | cat -n | tr -s ' ' > ${WAY_BKP}/fontes/lista_fontes.txt
 ls -1 ${WAY_ICONES} | cat -n | tr -s ' ' > ${WAY_BKP}/icones/lista_icones.txt
 ls -1 ${WAY_TEMAS} | cat -n | tr -s ' ' > ${WAY_BKP}/temas/lista_temas.txt
-cp -ru ${WAY_VSCODECFG}/* ${WAY_BKP}/vscode/configuracoes
+cp -r ${WAY_VSCODECFG}/* ${WAY_BKP}/vscode/configuracoes
 cp -ru ${WAY_VSCODEEXT}/* ${WAY_BKP}/vscode/extensoes
+cp -r ${WAY_NOTI}/* ${WAY_BKP}/noti
