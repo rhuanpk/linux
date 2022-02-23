9286168fccd315b5aec2fb7afa82dc3048fb4b6e
Rhuan Patriky <rhuan.pksf@gmail.com>
2022-02-23 11:47:38 -0300

    standard_scripts/.pessoal/rm_mais.sh: v2: agora removendo além dos "+" no início das linhas, remove as linhas em branco em baixo delas

--- standard_scripts/.pessoal/rm_mais.sh
+++ standard_scripts/.pessoal/rm_mais.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
-sed 's/^[[:blank:]]*+//g' ${1}
+
+file=${1}
+
+for i in $(seq $(grep -n '^[[:blank:]]*+' ${file} | wc -l)); do
+	line=$(grep -n '^[[:blank:]]*+' ${file} | sed -n "${i}p" | cut -c '1')
+	next_line=$((${line}+1))
+	cont_next_line=$(sed -n "${next_line}p" ${file})
+	[ -z ${cont_next_line:+foobar} ] && sed -i "${next_line}d" ${file}
+done
+
+sed -i 's/^[[:blank:]]*+//g' ${file}
