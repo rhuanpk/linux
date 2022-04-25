e23a99da00d3053806c26c55dc85281187fae01d
rhuan-pk <rhuan.pksf@gmail.com>
2022-04-25 20:13:22 -0300

    melhorado pick_bkp_file.sh

--- standard_scripts/pick_bkp_file.sh
+++ standard_scripts/pick_bkp_file.sh
@@ -2,17 +2,21 @@
 
 WAY_BKP="/home/${USER}/Documents/config_files_backup"
 WAY_OPT="/opt"
-WAY_FONTES="/home/${USER}/Documents/fontes"
+WAY_FONTES="/home/${USER}/Documents/fonts"
 WAY_ICONES="/home/${USER}/.icons"
 WAY_TEMAS="/home/${USER}/.themes"
-WAY_VSCODECFG="/home/${USER}/.config/Code/User"
-WAY_VSCODEEXT="/home/${USER}/.vscode/extensions"
-WAY_NOTI="/home/${USER}/.config/noti"
+WAY_TERMINATOR="/home/${USER}/.config/terminator/config"
+
+ARR_WAY=("${WAY_BKP}/opt" "${WAY_BKP}/fontes" "${WAY_BKP}/icones_temas" "${WAY_BKP}/terminator" "${WAY_BKP}/dpkg" "${WAY_BKP}/neofetch")
+
+for index in ${ARR_WAY[@]}; do
+	[ ! -d "${ARR_WAY}" ] && mkdir -p "${ARR_WAY}"
+done
 
 ls -1 ${WAY_OPT} | cat -n | tr -s ' ' > ${WAY_BKP}/opt/programas_opt.txt
 ls -1 ${WAY_FONTES} | cat -n | tr -s ' ' > ${WAY_BKP}/fontes/fontes.txt
 ls -1 ${WAY_ICONES} | cat -n | tr -s ' ' > ${WAY_BKP}/icones_temas/icones.txt
 ls -1 ${WAY_TEMAS} | cat -n | tr -s ' ' > ${WAY_BKP}/icones_temas/temas.txt
-cp -r ${WAY_VSCODECFG}/* ${WAY_BKP}/vscode/configuracoes
-cp -ru ${WAY_VSCODEEXT}/* ${WAY_BKP}/vscode/extensoes
-cp -r ${WAY_NOTI}/* ${WAY_BKP}/noti
+cp ${WAY_TERMINATOR} > ${WAY_BKP}/terminator/config.txt
+dpkg -l | tee ${WAY_BKP}/dpkg/list.txt
+neofetch > ${WAY_BKP}/neofetch/infos.txt
