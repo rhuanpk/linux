8eb7d694b1165c2f650784a5681dcf667db25896
Rhuan Patriky <rhuan.pksf@gmail.com>
2022-04-27 09:46:31 -0300

    standard_scripts/pick_bkp_file.sh: finalizado!

--- standard_scripts/pick_bkp_file.sh
+++ standard_scripts/pick_bkp_file.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/env bash
 
+# crontab:
+# */30 * * * * /usr/local/bin/pick_bkp_file 2>/tmp/cron_error.log
+
+USER='user'
 WAY_BKP="/home/${USER}/Documents/config_files_backup"
 WAY_OPT="/opt"
 WAY_FONTES="/home/${USER}/Documents/fonts"
 WAY_ICONES="/home/${USER}/.icons"
 WAY_TEMAS="/home/${USER}/.themes"
 WAY_TERMINATOR="/home/${USER}/.config/terminator/config"
+WAY_EXECUTABLES="/home/${USER}/others/executables"
 
-ARR_WAY=("${WAY_BKP}/opt" "${WAY_BKP}/fonts" "${WAY_BKP}/icons_themes" "${WAY_BKP}/terminator" "${WAY_BKP}/dpkg" "${WAY_BKP}/neofetch")
+ARR_WAY=("${WAY_BKP}/opt" "${WAY_BKP}/fonts" "${WAY_BKP}/icons_themes" "${WAY_BKP}/terminator" "${WAY_BKP}/dpkg" "${WAY_BKP}/neofetch" "${WAY_BKP}/executables")
 
 for index in ${ARR_WAY[@]}; do
 	[ ! -d "${index}" ] && mkdir -p "${index}"
 done
 
-ls -1 ${WAY_OPT} | cat -n | tr -s ' ' > ${WAY_BKP}/opt/programas_opt.txt
-ls -1 ${WAY_FONTES} | cat -n | tr -s ' ' > ${WAY_BKP}/fonts/fonts.txt
-ls -1 ${WAY_ICONES} | cat -n | tr -s ' ' > ${WAY_BKP}/icons_themes/icons.txt
-ls -1 ${WAY_TEMAS} | cat -n | tr -s ' ' > ${WAY_BKP}/icons_themes/themes.txt
-cp ${WAY_TERMINATOR} > ${WAY_BKP}/terminator/config.txt
-dpkg -l | tee ${WAY_BKP}/dpkg/list.txt
-neofetch > ${WAY_BKP}/neofetch/infos.txt
+ls -1 "${WAY_OPT}" | cat -n | tr -s ' ' > "${WAY_BKP}/opt/programas_opt.txt"
+ls -1 "${WAY_FONTES}" | cat -n | tr -s ' ' > "${WAY_BKP}/fonts/fonts.txt"
+ls -1 "${WAY_ICONES}" | cat -n | tr -s ' ' > "${WAY_BKP}/icons_themes/icons.txt"
+ls -1 "${WAY_TEMAS}" | cat -n | tr -s ' ' > "${WAY_BKP}/icons_themes/themes.txt"
+ls -1 "${WAY_EXECUTABLES}" | cat -n | tr -s ' ' > "${WAY_BKP}/executables/exebutables.txt"
+cp "${WAY_TERMINATOR}" "${WAY_BKP}/terminator/config.txt"
+dpkg -l | tee "${WAY_BKP}/dpkg/list.txt"
+neofetch > "${WAY_BKP}/neofetch/infos.txt"
