5f69db3e9b9121ed8fe227f407f316dcb252a6fb
rhuan-pk <rhuan.pksf@gmail.com>
2022-04-26 21:46:15 -0300

    standard_scripts/pick_bkp_file.sh: atualizado os caminhos

--- standard_scripts/pick_bkp_file.sh
+++ standard_scripts/pick_bkp_file.sh
@@ -7,16 +7,16 @@ WAY_ICONES="/home/${USER}/.icons"
 WAY_TEMAS="/home/${USER}/.themes"
 WAY_TERMINATOR="/home/${USER}/.config/terminator/config"
 
-ARR_WAY=("${WAY_BKP}/opt" "${WAY_BKP}/fontes" "${WAY_BKP}/icones_temas" "${WAY_BKP}/terminator" "${WAY_BKP}/dpkg" "${WAY_BKP}/neofetch")
+ARR_WAY=("${WAY_BKP}/opt" "${WAY_BKP}/fonts" "${WAY_BKP}/icons_themes" "${WAY_BKP}/terminator" "${WAY_BKP}/dpkg" "${WAY_BKP}/neofetch")
 
 for index in ${ARR_WAY[@]}; do
 	[ ! -d "${index}" ] && mkdir -p "${index}"
 done
 
 ls -1 ${WAY_OPT} | cat -n | tr -s ' ' > ${WAY_BKP}/opt/programas_opt.txt
-ls -1 ${WAY_FONTES} | cat -n | tr -s ' ' > ${WAY_BKP}/fontes/fontes.txt
-ls -1 ${WAY_ICONES} | cat -n | tr -s ' ' > ${WAY_BKP}/icones_temas/icones.txt
-ls -1 ${WAY_TEMAS} | cat -n | tr -s ' ' > ${WAY_BKP}/icones_temas/temas.txt
+ls -1 ${WAY_FONTES} | cat -n | tr -s ' ' > ${WAY_BKP}/fonts/fonts.txt
+ls -1 ${WAY_ICONES} | cat -n | tr -s ' ' > ${WAY_BKP}/icons_themes/icons.txt
+ls -1 ${WAY_TEMAS} | cat -n | tr -s ' ' > ${WAY_BKP}/icons_themes/themes.txt
 cp ${WAY_TERMINATOR} > ${WAY_BKP}/terminator/config.txt
 dpkg -l | tee ${WAY_BKP}/dpkg/list.txt
 neofetch > ${WAY_BKP}/neofetch/infos.txt
