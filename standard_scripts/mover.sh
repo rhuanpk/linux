ae689427050dcddeb6c38521436c7bc9308e91bc
rhuan-pk <rhuan.pksf@gmail.com>
2022-05-03 05:06:03 -0300

    standard_scripts/mover.sh: atualizado

--- standard_scripts/mover.sh
+++ standard_scripts/mover.sh
@@ -2,4 +2,6 @@
 
 # Move os scripts para a pasta correta
 
-[ ${#} -eq 0 ] && sudo cp -v ./*.sh /usr/local/bin/ || for files in ${@}; do sudo cp -v ./${files} /usr/local/bin/; done
+std_scripts_path=${HOME}/Documents/git/comandos-linux/standard_scripts
+
+[ ${#} -eq 0 ] && sudo cp -v ${std_scripts_path}/*.sh /usr/local/bin/ || for file in ${@}; do sudo cp -v ${std_scripts_path}/${file} /usr/local/bin/; done
