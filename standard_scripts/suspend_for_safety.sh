85e704972360c8ed90bdded79d47518f3a487e53
rhuan-pk <rhuan.pksf@gmail.com>
2022-05-04 23:41:51 -0300

    standard_scripts/suspend_for_safety.sh: alterado os valores de verificação pois apararentemente o acpi não é tão "preciso"!

--- standard_scripts/suspend_for_safety.sh
+++ standard_scripts/suspend_for_safety.sh
@@ -6,4 +6,4 @@
 
 battery_power=$(acpi -b | cut -d ',' -f '2' | sed 's/ \|%//g')
 
-[ ${battery_power} -le 7 ] && sudo systemctl suspend || [ ${battery_power} -le 9 ] && notify-send 'Battery Power low\'s!' 'Low battery: 98%, plug it into outlet.'
+[ ${battery_power} -le 9 ] && sudo systemctl suspend || [ ${battery_power} -le 11 ] && notify-send 'Battery Power low\'s!' 'Low battery: 11%, plug it into outlet.'
