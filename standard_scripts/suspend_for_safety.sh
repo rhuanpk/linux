f1a550167d65ac4de945f81b8deb53bf33004595
rhuan-pk <rhuan.pksf@gmail.com>
2022-05-08 10:07:02 -0300

    standard_scripts/suspend_for_safety.sh: atualizado com ifs e verifica se está plugado

--- standard_scripts/suspend_for_safety.sh
+++ standard_scripts/suspend_for_safety.sh
@@ -2,8 +2,16 @@
 
 # sudo apt install sudo acpi notify-send -y
 # sudo visudo -f /etc/sudoers.d/users
-# >>> "rhuan ALL=NOPASSWD:/usr/bin/systemctl"
+#    > "rhuan ALL=NOPASSWD:/usr/bin/systemctl"
+# */2 * * * * /usr/local/bin/pk-suspend_for_safety 2>/tmp/cron_error.log
 
 battery_power=$(acpi -b | cut -d ',' -f '2' | sed 's/ \|%//g')
+[ "$(acpi --ac-adapter | tr -d ' ' | cut -d ':' -f '2')" = 'on-line' ] && is_pluged=true || is_pugled=false
 
-[ ${battery_power} -le 9 ] && sudo systemctl suspend || [ ${battery_power} -le 11 ] && notify-send 'Battery Power low\'s!' 'Low battery: 11%, plug it into outlet.'
+if ! ${is_pluged}; then
+	if [ ${battery_power} -le 9 ]; then
+		sudo systemctl suspend
+	elif [ ${battery_power} -le 11 ]; then
+		notify-send 'Battery Power low!' 'Low battery: 11% or less, plug it into outlet.'
+	fi
+fi
