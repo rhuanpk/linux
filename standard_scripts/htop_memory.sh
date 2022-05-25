3bff8f596f1d8967b7262effbfe48c8749df78b5
Rhuan Patriky <rhuan.pksf@gmail.com>
2022-05-25 14:38:38 -0300

    standard_scripts/htop_memory.sh: alterado o vlaor da soma de 27 para 100 de memória

--- standard_scripts/htop_memory.sh
+++ standard_scripts/htop_memory.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-echo "RAM: $(($(free -m | tr -s ' ' | head -n 2 | tail -n 1 | cut -d ' ' -f 3)+27)) mb"
+echo "RAM: $(($(free -m | tr -s ' ' | head -n 2 | tail -n 1 | cut -d ' ' -f 3)+100)) mb"
