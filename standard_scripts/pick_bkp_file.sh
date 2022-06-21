900ef37c625287c78efde53baee746fa552f5bcb
rhuan-pk <rhuan.pksf@gmail.com>
2022-06-21 07:18:36 -0300

    standard_scripts/i3suspend.sh: chamado a suspensão com "sudo"; standard_scripts/pick_bkp_file.sh: continuando atualizações

--- standard_scripts/pick_bkp_file.sh
+++ standard_scripts/pick_bkp_file.sh
@@ -29,28 +29,39 @@ verify_privileges
 
 # >>>>> PROGRAM START <<<<<
 
-USER='user'
-WAY_BKP="/home/${USER}/Documents/config_files_backup"
-WAY_OPT="/opt"
-WAY_FONTES="/home/${USER}/Documents/fonts"
-WAY_ICONES="/home/${USER}/.icons"
-WAY_TEMAS="/home/${USER}/.themes"
-WAY_TERMINATOR="/home/${USER}/.config/terminator/config"
-WAY_EXECUTABLES="/home/${USER}/others/executables"
-WAY_TREE="/home/${USER}/others"
+user=user
+home=/home/${user}
+PATH_BKP=${home}/Documents/config_files_backup
 
-ARR_WAY=("${WAY_BKP}/opt" "${WAY_BKP}/fonts" "${WAY_BKP}/icons_themes" "${WAY_BKP}/terminator" "${WAY_BKP}/dpkg" "${WAY_BKP}/neofetch" "${WAY_BKP}/executables" "${WAY_BKP}/others")
+PATH_OPT=/opt
+PATH_FONTES=${home}/Documents/fonts
+PATH_ICONES=${home}/.icons
+PATH_TEMAS=${home}/.themes
+PATH_TERMINATOR=${home}/.config/terminator/config
+PATH_EXECUTABLES=${home}/others/executables
+PATH_TREE=${home}/others
 
-for index in ${ARR_WAY[@]}; do
-	[ ! -d "${index}" ] && mkdir -p "${index}"
+ARR_PATH=(\
+	${PATH_BKP}/opt \
+	${PATH_BKP}/fonts \
+	${PATH_BKP}/icons_themes \
+	${PATH_BKP}/terminator \
+	${PATH_BKP}/dpkg \
+	${PATH_BKP}/neofetch \
+	${PATH_BKP}/executables \
+	${PATH_BKP}/others\
+)
+
+for path in ${ARR_PATH[@]}; do
+	[ ! -d ${path} ] && mkdir -p ${path}
 done
 
-ls -1 "${WAY_OPT}" | cat -n | tr -s ' ' > "${WAY_BKP}/opt/programas_opt.txt"
-ls -1 "${WAY_FONTES}" | cat -n | tr -s ' ' > "${WAY_BKP}/fonts/fonts.txt"
-ls -1 "${WAY_ICONES}" | cat -n | tr -s ' ' > "${WAY_BKP}/icons_themes/icons.txt"
-ls -1 "${WAY_TEMAS}" | cat -n | tr -s ' ' > "${WAY_BKP}/icons_themes/themes.txt"
-ls -1 "${WAY_EXECUTABLES}" | cat -n | tr -s ' ' > "${WAY_BKP}/executables/exebutables.txt"
-cp "${WAY_TERMINATOR}" "${WAY_BKP}/terminator/config.txt"
-dpkg -l | tee "${WAY_BKP}/dpkg/list.txt"
-neofetch > "${WAY_BKP}/neofetch/infos.txt"
-tree "${WAY_TREE}" > "${WAY_BKP}/others/tree_output.txt"
+ls -1 ${PATH_OPT} | cat -n | tr -s ' ' > ${PATH_BKP}/opt/programas_opt.txt
+ls -1 ${PATH_FONTES} | cat -n | tr -s ' ' > ${PATH_BKP}/fonts/fonts.txt
+ls -1 ${PATH_ICONES} | cat -n | tr -s ' ' > ${PATH_BKP}/icons_themes/icons.txt
+ls -1 ${PATH_TEMAS} | cat -n | tr -s ' ' > ${PATH_BKP}/icons_themes/themes.txt
+ls -1 ${PATH_EXECUTABLES} | cat -n | tr -s ' ' > ${PATH_BKP}/executables/exebutables.txt
+cp ${PATH_TERMINATOR} ${PATH_BKP}/terminator/config.txt
+dpkg -l | tee ${PATH_BKP}/dpkg/list.txt
+neofetch > ${PATH_BKP}/neofetch/infos.txt
+tree ${PATH_TREE} > ${PATH_BKP}/others/tree_output.txt
