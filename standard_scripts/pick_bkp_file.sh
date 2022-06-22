307ecaedf7e6b5da6bc1aa472c3f62d3605cd179
rhuan-pk <rhuan.pksf@gmail.com>
2022-06-22 06:53:37 -0300

    standard_scripts/pick_bkp_file.sh: continuado atualizações

--- standard_scripts/pick_bkp_file.sh
+++ standard_scripts/pick_bkp_file.sh
@@ -31,9 +31,9 @@ verify_privileges
 
 user=user
 home=/home/${user}
-PATH_BKP=${home}/Documents/config_files_backup
+path_bkp=${home}/Documents/config_files_backup
 
-PATH_OPT=/opt
+path_opt=/opt
 PATH_FONTES=${home}/Documents/fonts
 PATH_ICONES=${home}/.icons
 PATH_TEMAS=${home}/.themes
@@ -42,26 +42,26 @@ PATH_EXECUTABLES=${home}/others/executables
 PATH_TREE=${home}/others
 
 ARR_PATH=(\
-	${PATH_BKP}/opt \
-	${PATH_BKP}/fonts \
-	${PATH_BKP}/icons_themes \
-	${PATH_BKP}/terminator \
-	${PATH_BKP}/dpkg \
-	${PATH_BKP}/neofetch \
-	${PATH_BKP}/executables \
-	${PATH_BKP}/others\
+	${path_bkp}/opt \
+	${path_bkp}/fonts \
+	${path_bkp}/icons_themes \
+	${path_bkp}/terminator \
+	${path_bkp}/dpkg \
+	${path_bkp}/neofetch \
+	${path_bkp}/executables \
+	${path_bkp}/others\
 )
 
 for path in ${ARR_PATH[@]}; do
 	[ ! -d ${path} ] && mkdir -p ${path}
 done
 
-ls -1 ${PATH_OPT} | cat -n | tr -s ' ' > ${PATH_BKP}/opt/programas_opt.txt
-ls -1 ${PATH_FONTES} | cat -n | tr -s ' ' > ${PATH_BKP}/fonts/fonts.txt
-ls -1 ${PATH_ICONES} | cat -n | tr -s ' ' > ${PATH_BKP}/icons_themes/icons.txt
-ls -1 ${PATH_TEMAS} | cat -n | tr -s ' ' > ${PATH_BKP}/icons_themes/themes.txt
-ls -1 ${PATH_EXECUTABLES} | cat -n | tr -s ' ' > ${PATH_BKP}/executables/exebutables.txt
-cp ${PATH_TERMINATOR} ${PATH_BKP}/terminator/config.txt
-dpkg -l | tee ${PATH_BKP}/dpkg/list.txt
-neofetch > ${PATH_BKP}/neofetch/infos.txt
-tree ${PATH_TREE} > ${PATH_BKP}/others/tree_output.txt
+ls -1 ${path_opt} | cat -n | tr -s ' ' > ${path_bkp}/opt/programas_opt.txt
+ls -1 ${PATH_FONTES} | cat -n | tr -s ' ' > ${path_bkp}/fonts/fonts.txt
+ls -1 ${PATH_ICONES} | cat -n | tr -s ' ' > ${path_bkp}/icons_themes/icons.txt
+ls -1 ${PATH_TEMAS} | cat -n | tr -s ' ' > ${path_bkp}/icons_themes/themes.txt
+ls -1 ${PATH_EXECUTABLES} | cat -n | tr -s ' ' > ${path_bkp}/executables/exebutables.txt
+cp ${PATH_TERMINATOR} ${path_bkp}/terminator/config.txt
+dpkg -l | tee ${path_bkp}/dpkg/list.txt
+neofetch > ${path_bkp}/neofetch/infos.txt
+tree ${PATH_TREE} > ${path_bkp}/others/tree_output.txt
