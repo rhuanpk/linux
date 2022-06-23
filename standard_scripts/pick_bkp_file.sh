9adf8558957572bd82996eef577d56f043c31be4
Rhuan Patriky <rhuan.pksf@gmail.com>
2022-06-23 18:11:53 -0300

    standard_scripts/pick_bkp_file.sh: finalizado organização do script

--- standard_scripts/pick_bkp_file.sh
+++ standard_scripts/pick_bkp_file.sh
@@ -31,37 +31,37 @@ verify_privileges
 
 user=user
 home=/home/${user}
-path_bkp=${home}/Documents/config_files_backup
+bkp_path=${home}/Documents/config_files_backup
 
 path_opt=/opt
-PATH_FONTES=${home}/Documents/fonts
-PATH_ICONES=${home}/.icons
-PATH_TEMAS=${home}/.themes
-PATH_TERMINATOR=${home}/.config/terminator/config
-PATH_EXECUTABLES=${home}/others/executables
-PATH_TREE=${home}/others
+path_fontes=${home}/Documents/fonts
+path_icones=${home}/.icons
+path_temas=${home}/.themes
+path_terminator=${home}/.config/terminator/config
+path_tree=${home}/others
+path_executables=${home}/others/executables
 
-ARR_PATH=(\
-	${path_bkp}/opt \
-	${path_bkp}/fonts \
-	${path_bkp}/icons_themes \
-	${path_bkp}/terminator \
-	${path_bkp}/dpkg \
-	${path_bkp}/neofetch \
-	${path_bkp}/executables \
-	${path_bkp}/others\
+declare -A arr_path=(\
+	[opt]=${bkp_path}/opt \
+	[fon]=${bkp_path}/fonts \
+	[ico_the]=${bkp_path}/icons_themes \
+	[ter]=${bkp_path}/terminator \
+	[dpk]=${bkp_path}/dpkg \
+	[neo]=${bkp_path}/neofetch \
+	[exe]=${bkp_path}/executables \
+	[oth]=${bkp_path}/others\
 )
 
-for path in ${ARR_PATH[@]}; do
+for path in ${arr_path[@]}; do
 	[ ! -d ${path} ] && mkdir -p ${path}
 done
 
-ls -1 ${path_opt} | cat -n | tr -s ' ' > ${path_bkp}/opt/programas_opt.txt
-ls -1 ${PATH_FONTES} | cat -n | tr -s ' ' > ${path_bkp}/fonts/fonts.txt
-ls -1 ${PATH_ICONES} | cat -n | tr -s ' ' > ${path_bkp}/icons_themes/icons.txt
-ls -1 ${PATH_TEMAS} | cat -n | tr -s ' ' > ${path_bkp}/icons_themes/themes.txt
-ls -1 ${PATH_EXECUTABLES} | cat -n | tr -s ' ' > ${path_bkp}/executables/exebutables.txt
-cp ${PATH_TERMINATOR} ${path_bkp}/terminator/config.txt
-dpkg -l | tee ${path_bkp}/dpkg/list.txt
-neofetch > ${path_bkp}/neofetch/infos.txt
-tree ${PATH_TREE} > ${path_bkp}/others/tree_output.txt
+ls -1 ${path_opt} | cat -n | tr -s ' ' >${arr_path[opt]}/programas_opt.txt
+ls -1 ${path_fontes} | cat -n | tr -s ' ' >${arr_path[fon]}/fonts.txt
+ls -1 ${path_icones} | cat -n | tr -s ' ' >${arr_path[ico_the]}/icons.txt
+ls -1 ${path_temas} | cat -n | tr -s ' ' >${arr_path[ico_the]}/themes.txt
+ls -1 ${path_executables} | cat -n | tr -s ' ' >${arr_path[exe]}/exebutables.txt
+cp ${path_terminator} ${arr_path[ter]}/config.txt
+dpkg -l >${arr_path[dpk]}/list.txt
+neofetch >${arr_path[neo]}/infos.txt
+tree ${path_tree} >${arr_path[oth]}/tree_output.txt
