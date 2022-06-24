7316008e8131e9d5dea84ef60808690d1a97080e
Rhuan Patriky <rhuan.pksf@gmail.com>
2022-06-24 12:25:38 -0300

    add: standard_scripts/sc2cb.sh

---
+++ standard_scripts/sc2cb.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# sc2cb = Send Color to ClipBoard
+# Envia o código hexa da cor para a sua área de transferência
+
+verify_privileges() {
+        if [ ${UID} -eq 0 ]; then
+                echo -e "ERROR: Run this program without privileges!\nExiting..."
+                exit 1
+        fi
+}
+
+print_usage() {
+        echo -e "Run:\n\t./$(basename ${0})"
+}
+
+# verify_privileges
+
+[ ${#} -ge 1 -o "${1,,}" = '-h' -o "${1,,}" = '--help' ] && {
+        print_usage
+        exit 1
+}
+
+# >>>>> PROGRAM START <<<<<
+
+echo \#$(colorpicker --one-shot --short | cut -d '#' -f 2) | tr -d '\n' | xsel -b
