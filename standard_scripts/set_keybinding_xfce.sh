0571918c68b003d694341f2858491f8407425abf
rhuan-pk <rhuan.pksf@gmail.com>
2022-07-03 02:38:29 -0300

    add: set_keybinding_xfce.sh; set_keybinding_gnome.sh: alterado o nome desse para esse mesmo e feitos melhorias de apresentação

---
+++ standard_scripts/set_keybinding_xfce.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# Reset and set as new keybing
+
+verify_privileges() {
+        if [ ${UID} -eq 0 ]; then
+                echo -e "ERROR: Run this program without privileges!\nExiting..."
+                exit 1
+        fi
+}
+
+print_usage() {
+	cat <<- EOF
+		#######################################################################
+		#
+		# >>> $(basename ${0^^})!
+		#
+		# Set a new binding passing the following params for xfce systems:
+		#
+		# 1. Binding
+		# 2. Command
+		#
+		# Example:
+		#
+		# 	$(basename ${0}) '<Alt>v' vcontrol.sh
+		#
+		# For remove a binding pass only a param:
+		#
+		# 1. Binding
+		#
+		# Example:
+		#
+		# 	$(basename ${0}) '<Alt>v'
+		#
+		#######################################################################
+	EOF
+}
+
+verify_privileges
+
+[ ${#} -lt 1 -o "${1,,}" = '-h' -o "${1,,}" = '--help' ] && {
+        print_usage
+        exit 1
+}
+
+# >>>>> PROGRAM START <<<<<
+
+xfconf-query --reset --channel xfce4-keyboard-shortcuts --property "/xfwm4/custom/${1}"
+xfconf-query --reset --channel xfce4-keyboard-shortcuts --property "/commands/custom/${1}"
+[ ${#} -ge 2 ] && xfconf-query --create --channel xfce4-keyboard-shortcuts --property "/commands/custom/${1}" --type string --set "${2}"
