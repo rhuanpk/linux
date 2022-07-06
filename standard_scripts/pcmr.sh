0ae076755ba49e8afa96e2bb961231e261695f28
Rhuan Patriky <rhuan.pksf@gmail.com>
2022-07-06 15:15:53 -0300

    v2: standard_scripts/pcmr.sh: implementado com mais conhecimento

--- standard_scripts/pcmr.sh
+++ standard_scripts/pcmr.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env zsh
 
-broken_flag=true
-if ${broken_flag}; then
-	echo "Esse script precisa de ajustes! (caso queria usar mesmo assim, edite a flag como \"false\" dentro do script)\nSaindo..."
-	exit 1
-fi
-
 # Pega o consumo de RAM de algum programa específico
 
 verify_privileges() {
@@ -16,29 +10,42 @@ verify_privileges() {
 }
 
 print_usage() {
-        echo -e "Passe o nome de algum programa para saber o consumo de RAM do mesmo.\n\tExemplo: ./$(basename ${0}) google"
+        echo -e "Passe o nome de algum programa para saber o consumo de RAM do mesmo.\n\tExemplo: ./$(basename ${0}) [-f|--full] chrome"
 }
 
 verify_privileges
 
-[ ${#} -ge 1 -o "${1}" = '-h' -o "${1}" = '--help' ] && {
+[ ${#} -lt 1 -o "${1}" = '-h' -o "${1}" = '--help' ] && {
         print_usage
         exit 1
 }
 
 # >>>>> PROGRAM START <<<<<
 
+# ---------- implementações -------------------------------------------------------------------------------------------------------------------
+#
+# 1. Fazer um cálculo para printar o resultado com cores diferente, pegar o tatal de RAM da máquina e caso seja 40% = verde, até 75% amarelo...
+#
+# ---------------------------------------------------------------------------------------------------------------------------------------------
+
+# broken_flag=true
+# if ${broken_flag}; then
+# 	echo "Esse script precisa de ajustes! (caso queria usar mesmo assim, edite a flag como \"false\" dentro do script)\nSaindo..."
+# 	exit 1
+# fi
+
 # Mensagem caso precise instalar o smem
-msg(){
+no_installed(){
         echo -e "\e[40;37;1m
-                \rscript: ---------------------------------------------- \n \
-                \rscript: |   Necessário instalar o programa: SMEM     | \n \
-                \rscript: |                                            | \n \
-                \rscript: |   Enter - Prosseguir para a instalação     | \n \
-                \rscript: |   Ctrl+c - Cancelar                        | \n \
-                \rscript: ---------------------------------------------- \e[m\n"
+                \r${0}: ---------------------------------------------- \n \
+                \r${0}: |   Necessário instalar o programa: SMEM     | \n \
+                \r${0}: |                                            | \n \
+                \r${0}: |   Enter - Prosseguir para a instalação     | \n \
+                \r${0}: |   Ctrl+c - Cancelar                        | \n \
+                \r${0}: ---------------------------------------------- \e[m\n"
 }
 
+# Converte o nome do programa em maiúsculo
 lower_to_upper(){
 	echo "${1}" | tr '[:lower:]' '[:upper:]'
 }
@@ -47,22 +54,20 @@ lower_to_upper(){
 # ---------------------------------------------------------------
 # Poderia chamar diretamente o "dpkg -l <program>" num if negado
 # ---------------------------------------------------------------
-INSTALADO=$(dpkg -l smem 2> /dev/null | tail -n 1 | cut -c 1-2)
-[ "${INSTALADO}" != "ii" ] && msg && read foobar && sudo apt install smem -y
+if ! (dpkg -l smem >/dev/null 2>&1); then
+        no_installed && \
+        read readkey && \
+        sudo apt install smem -y
+fi
 
-# Cria e entra no diretório temporário
-mkdir -p /tmp/tmp; cd /tmp/tmp
-
-# Reseta o valor da variável SOMA
-SOMA=0
-
-# Cria o arquivo aleatório temporário
-TMPFILE=$(mktemp RAM-XXXXX.tmp)
-
-# INICIO DO POGRAMA
-echo "$(smem -rtk | grep "${1}")" > ${TMPFILE}
-tr 'M' ',' < ${TMPFILE} | cut -d ',' -f 2 | tr -d '[:blank:]' | sed '/^$/d' | tr 'OK' '#' | sed '/#/d' > ${TMPFILE}
-for line in $(seq `wc -l ${TMPFILE} | cut -d ' ' -f 1`); do let SOMA+=$(head -n ${line} ${TMPFILE} | tail -n 1); done
-
-# Printa o resultado na tela
-echo -e "RAM - \e[1m$(lower_to_upper ${1})\e[m: $(echo "${SOMA}" | cut -d '.' -f 1)M"
+[ "${1}" = "-f" -o "${1}" = "--full" ] && {
+        if ! (dpkg -l ${2} >/dev/null 2>&1); then
+                echo "Programa pode não existir..."
+        fi
+        smem -aktP ${2}
+} || {
+        if ! (dpkg -l ${1} >/dev/null 2>&1); then
+                echo "Programa pode não existir..."
+        fi
+        echo -e "\e[1m$(lower_to_upper ${1})\e[m: $(smem -aktP ${1} | tail -1 | tr -s ' ' | cut -d ' ' -f 5) (RAM)"
+}
