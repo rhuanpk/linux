76b638cc4782b49da7802e08126dc820d55adc08
rhuan-pk <rhuan.pksf@gmail.com>
2022-07-17 17:03:37 -0300

    standard_scripts/polybar_battery.sh: atualizado: caso variável de controle fique nula, retorne falso na hora do condicional

--- standard_scripts/polybar_battery.sh
+++ standard_scripts/polybar_battery.sh
@@ -2,6 +2,6 @@
 
 [ "$(acpi --ac-adapter | tr -d '[[:blank:]]' | cut -d ':' -f 2)" = 'power_supply' ] && is_power_supply=true
 
-if ! ${is_power_supply}; then
+if ! ${is_power_supply:-false}; then
 	echo "Battery: $(acpi | tr -d '[[:blank:]]' | cut -d ',' -f 2) |"
 fi
