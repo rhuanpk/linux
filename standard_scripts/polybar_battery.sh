88576ae78e85171391969f6f3f27ef41c3d4184c
Rhuan Patriky <rhuan.pksf@gmail.com>
2022-07-18 08:32:09 -0300

    standard_scripts/polybar_battery.sh: atualizado: conectado saida de erro do acpi no primeiro condicional com stdout para conseguir capturar se é ou não "power_supply"

--- standard_scripts/polybar_battery.sh
+++ standard_scripts/polybar_battery.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-[ "$(acpi --ac-adapter | tr -d '[[:blank:]]' | cut -d ':' -f 2)" = 'power_supply' ] && is_power_supply=true
+[ "$(acpi --ac-adapter 2>&1 | tr -d '[[:blank:]]' | cut -d ':' -f 2)" = 'power_supply' ] && is_power_supply=true
 
 if ! ${is_power_supply:-false}; then
 	echo "| Battery: $(acpi | tr -d '[[:blank:]]' | cut -d ',' -f 2) "
