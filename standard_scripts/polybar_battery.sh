990b33785f11fed733355d2f70489fbba6c068d8
Rhuan Patriky <rhuan.pksf@gmail.com>
2022-07-25 14:56:59 -0300

    standard_scripts/polybar_battery.sh: add: estrutura padrão

--- standard_scripts/polybar_battery.sh
+++ standard_scripts/polybar_battery.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 
+# Printa a porcentagem de bateria caso seja "ac adpter"
+
+verify_privileges() {
+        if [ ${UID} -eq 0 ]; then
+                echo -e "ERROR: Run this program without privileges!\nExiting..."
+                exit 1
+        fi
+}
+
+print_usage() {
+        echo -e "Run:\n\t./$(basename ${0})"
+}
+
+verify_privileges
+
+[ ${#} -ge 1 -o "${1,,}" = '-h' -o "${1,,}" = '--help' ] && {
+        print_usage
+        exit 1
+}
+
+# >>>>> PROGRAM START <<<<<
+
 [ "$(acpi --ac-adapter 2>&1 | tr -d '[[:blank:]]' | cut -d ':' -f 2)" = 'power_supply' ] && is_power_supply=true
 
 if ! ${is_power_supply:-false}; then
