6a998612729df7d75019b4108d7b9a3033c59d93
rhuan-pk <rhuan.pksf@gmail.com>
2022-08-02 09:30:59 -0300

    add: standard_scripts/todo_notify.sh; removed: standard_scripts/.pessoal/pick_bkp_file.sh

---
+++ standard_scripts/todo_notify.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Envia notificação para a área de trabalho para lembra do todo list
+
+verify_privileges() {
+        if [ ${UID} -eq 0 ]; then
+                echo -e "ERROR: Run this program without privileges!\nExiting..."
+                exit 1
+        fi
+}
+
+print_usage() {
+        echo -e "Run:\n\t./$(basename ${0})"
+}
+
+verify_privileges
+
+[ ${#} -ge 1 -o "${1,,}" = '-h' -o "${1,,}" = '--help' ] && {
+        print_usage
+        exit 1
+}
+
+# >>>>> PROGRAM START <<<<<
+
+# cron: * */1 * * * /usr/local/bin/pk-todo_notify 2>/tmp/cron_error.log
+
+message=$(cat ~/Documents/anotacoes/.todo_list.txt)
+notify-send '>>> ToDo List !' ${message}
