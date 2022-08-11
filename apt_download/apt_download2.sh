db062a55428836669f57ef42557ba9dee76b66a3
rhuan-pk <rhuan.pksf@gmail.com>
2022-08-11 01:07:30 -0300

    atualizando

--- apt_download/apt_download2.sh
+++ apt_download/apt_download2.sh
@@ -4,16 +4,18 @@ get_number_lines() {
 	echo $(wc -l < $file)
 }
 package=$1
-packages_file=/tmp/apt_download_packages.txt
-echo $package > $packages_file
+packages_file_main=/tmp/apt_download_packages_main.txt
+packages_files_tmp=/tmp/apt_download_packages_tmp.txt
+echo $package > $packages_file_tmp > $packages_file_main
 while :; do
-	for pkg in $(cat ${packages_file}); do
-		old_lines=$(get_number_lines $packages_file)
-		apt-cache depends $pkg | tr -d '^[:blank:]' | grep -iF depends | cut -d ':' -f 2 >> ${packages_file}
-		cat <<- eof > $packages_file
-			$(sort -u ${packages_file})
+	old_lines=$(get_number_lines $packages_file_main)
+	echo "" > ${all_packages_files_tmp}
+	for pkg in $(cat ${all_packages_files_tmp}); do
+		apt-cache depends $pkg | tr -d '^[:blank:]' | grep -iF depends | cut -d ':' -f 2 > ${packages_files_tmp} >> ${all_packages_files_tmp}
+		cat <<- eof > $packages_file_main
+			$(sort -u $(cat ${packages_file_main} ${packages_file_tmp}))
 		eof
-		new_lines=$(get_number_lines $packages_file)
-		[ $new_lines -le $old_lines ] && exit 0
 	done
+	new_lines=$(get_number_lines $packages_file_main)
+	[ $new_lines -le $old_lines ] && exit 0
 done
