0303938ce598aa5b13be2d8ba0703e3d516bd886
rhuan-pk <rhuan.pksf@gmail.com>
2022-08-10 22:29:20 -0300

    add novos arquivos

---
+++ apt_download/apt_download2.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+get_number_lines() {
+	file=$1
+	echo $(wc -l < $file)
+}
+package=$1
+packages_file=/tmp/apt_download_packages.txt
+echo $package > $packages_file
+while :; do
+	for pkg in $(cat ${packages_file}); do
+		old_lines=$(get_number_lines $packages_file)
+		apt-cache depends $pkg | tr -d '^[:blank:]' | grep -iF depends | cut -d ':' -f 2 >> ${packages_file}
+		cat <<- eof > $packages_file
+			$(sort -u ${packages_file})
+		eof
+		new_lines=$(get_number_lines $packages_file)
+		[ $new_lines -le $old_lines ] && exit 0
+	done
+done
