202139f40bc21567c6ca37106426c4dc35001160
rhuan-pk <rhuan.pksf@gmail.com>
2022-08-14 18:19:51 -0300

    standard_scripts/apt_download.sh: add: "zstd"

--- standard_scripts/apt_download.sh
+++ standard_scripts/apt_download.sh
@@ -30,13 +30,32 @@ verify_privileges
 
 # >>>>> PROGRAM START <<<<<
 
+define_work_dir() {
+	path_folder=$1
+	[ ! -d $path_folder ] && { mkdir $path_folder && cd $path_folder ;} || { cd $path_folder && rm -rf ./* ;}
+}
+
 package=$1
 work_path=/tmp/apt_download
+zstd_work_path=/tmp/apt_download/zstd
 packages_file=${work_path}/packages_file.txt
 
-[ ! -d $work_path ] && { mkdir $work_path && cd $work_path ;} || { cd $work_path && rm -rf ./* ;}
+define_work_dir $work_path
 
 apt-cache depends --no-recommends --no-suggests --no-conflicts --no-breaks --no-replaces --no-enhances $package > $packages_file
 apt-cache rdepends --no-recommends --no-suggests --no-conflicts --no-breaks --no-replaces --no-enhances  $package >> $packages_file
 
 tr -d '^[[:blank:]]' < $packages_file | grep -vF i386 | cut -d ':' -f 2 | grep -E '^\w' | sort -u | xargs sudo apt download -y
+
+define_work_dir $zstd_work_path
+
+cp -v ${work_path}/*.deb ${zstd_work_path}/
+for deb_file in $(ls -1); do
+	ar x $deb_file
+	unzstd control.tar.zst
+	unzstd data.tar.zst
+	xz control.tar
+	xz data.tar
+	rm $deb_file
+	ar cr $deb_file debian-binary control.tar.xz data.tar.xz
+done
