9b96f720fc0f0bd43f925feb8940f4d991f83384
rhuan-pk <rhu4n.arus@gmail.com>
2022-10-26 16:25:55 -0300

    standard_scripts/i3lock.sh: atualizado: quase nem a barra, só mostra se tiver errado a senha

--- standard_scripts/i3lock.sh
+++ standard_scripts/i3lock.sh
@@ -44,17 +44,17 @@ i3lock --color 00000000          \
 --bar-indicator                  \
 --bar-pos y+h                    \
 --bar-direction 1                \
---bar-max-height 5               \
---bar-base-width 5               \
+--bar-max-height 1               \
+--bar-base-width 1               \
 --bar-color 00000000             \
---keyhl-color 00ff94cc           \
---bshl-color ff0033cc            \
+--keyhl-color 00000000           \
+--bshl-color 00000000            \
 \
 --clock                          \
 --force-clock                    \
 --time-color ffffffff            \
 --date-color ffffffff            \
---ringver-color 00ff94cc         \
+--ringver-color 00000000         \
 --ringwrong-color ff0033cc       \
 \
 --time-str="%H:%M:%S"            \
