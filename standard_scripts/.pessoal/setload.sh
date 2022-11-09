30eb13bfc98c96a513461e460b017197a036de44
rhuan-pk <rhuan.pksf@gmail.com>
2022-11-08 21:54:36 -0300

    atualizado: standard_scripts/.pessoal/setload.sh: deixado mais flexível exportante somente até a raiz do repo.

--- standard_scripts/.pessoal/setload.sh
+++ standard_scripts/.pessoal/setload.sh
@@ -3,7 +3,6 @@
 setload() {
 	variable_name=${1:?'need a variable to set!'}
 	repo_name=${2:?'needs the name of the repository to be loaded!'}
-	file_load=${3:?'needs the path of the file to be loaded!'}
 	home=/home/$(id -nu 1000)
         environment_path=/etc/environment
 	full_path=$(
@@ -11,15 +10,13 @@ setload() {
 		find ${home}/ -type f -name ".way_flag_${repo_name}.cfg" 2>&- | \
 		sed -E "/${folders:-$(uuidgen)}/d" | \
 		xargs dirname
-	)/${file_load}
+	)
 	line_and_path=$(grep -nE "${variable_name}=.*$" ${environment_path:?'whats the environment path?'})
 	if ! sudo sed -i "${line_and_path%:*}s~${line_and_path#*=}~${full_path}~" ${environment_path}; then
 		echo $full_path
 	fi
 }
 
-# variable=${PK_LOAD_ZBASHRC:-$(setload.sh | grep -oF standard_scripts)}
-
-setload PK_LOAD_ZBASHRC cfg-bkp rc/zbashrc
-setload PK_LOAD_STANDARDSCRIPTS comandos-linux standard_scripts
-setload PK_LOAD_STANDARDUTILS pkutils bash/
+setload PK_LOAD_CFGBKP cfg-bkp
+setload PK_LOAD_LINUXCOMMANDS comandos-linux
+setload PK_LOAD_PKUTILS pkutils
