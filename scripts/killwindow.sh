7317ab6b0ad889581a35b22fc75574d74dd78222
rhuan-pk <rhuan.pksf@gmail.com>
2023-03-01 02:24:36 -0300

    add: "scripts/killwindow.sh".

---
+++ scripts/killwindow.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# killwindow launch a selector for you select a specific window to kill.
+
+# >>> variable declarations !
+
+script=$(basename "${0}")
+home=${HOME:-/home/${USER:-$(whoami)}}
+
+# >>> function declarations !
+
+verify_privileges() {
+	[ $UID -eq 0 ] && {
+		echo -e "ERROR: Run this program without privileges!\nExiting..."
+		exit 1
+	}
+}
+
+print_usage() {
+        echo -e "Run:\n\t./${script}"
+}
+
+# >>> pre statements !
+
+set +o histexpand
+
+verify_privileges
+[ $# -ge 1 -o "${1,,}" = '-h' -o "${1,,}" = '--help' ] && {
+        print_usage
+        exit 1
+}
+
+# >>> *** PROGRAM START *** !
+xdotool windowkill `xdotool selectwindow`
