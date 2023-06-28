f3de090ef1e7474e6e2ea1e539506dd88cf2b9c6
rhuan-pk <rhuan.pksf@gmail.com>
2023-06-28 15:43:06 -0300

    style: alterado script para os padrões.

--- scripts/pick_bkp_file.sh
+++ scripts/pick_bkp_file.sh
@@ -33,55 +33,61 @@ verify_privileges
 # >>> *** PROGRAM START *** !
 
 cleanup-history() {
-	for file in "${array_pathway_backup[history]}"/*; do
+	for file in "${ARRAY_PATHWAY_BACKUP['history']}"/*; do
 		find "$file" -mtime +2 -exec rm '{}' \;
 	done
 }
 
-path_backup=${home}/Documents/config_files_backup/`hostname`
-declare -A array_pathway_backup=(\
-	[opt]=${path_backup}/opt \
-	[fonts]=${path_backup}/fonts \
-	[iconthemes]=${path_backup}/icons_themes \
-	[terminator]=${path_backup}/terminator \
-	[dpkg]=${path_backup}/dpkg \
-	[neofetch]=${path_backup}/neofetch \
-	[others]=${path_backup}/others \
-	[gtk]=${path_backup}/gtk \
-	[localbin]=${path_backup}/local_bin \
-	[history]=${path_backup}/history\
+PATHWAY_BACKUP="$home/Documents/config_files_backup/`hostname`"
+declare -A ARRAY_PATHWAY_BACKUP=( \
+	['opt']="$PATHWAY_BACKUP/opt" \
+	['fonts']="$PATHWAY_BACKUP/fonts" \
+	['iconthemes']="$PATHWAY_BACKUP/icons_themes" \
+	['terminator']="$PATHWAY_BACKUP/terminator" \
+	['dpkg']="$PATHWAY_BACKUP/dpkg" \
+	['neofetch']="$PATHWAY_BACKUP/neofetch" \
+	['others']="$PATHWAY_BACKUP/others" \
+	['gtk']="$PATHWAY_BACKUP/gtk" \
+	['localbin']="$PATHWAY_BACKUP/local_bin" \
+	['history']="$PATHWAY_BACKUP/history" \
 )
 
-for pathway in ${array_pathway_backup[@]}; do
-	[ ! -d "${pathway}" ] && mkdir -p "${pathway}"
+for pathway in ${ARRAY_PATHWAY_BACKUP[@]}; do
+	[ ! -d "$pathway/" ] && mkdir -p "$pathway/"
 done
 
-path_opt=/opt
-path_fonts=${home}/Documents/fonts
-path_icons=${home}/.icons
-path_themes=${home}/.themes
-path_terminator=${home}/.config/terminator/config
-path_tree=${home}/others
-path_gtk=${home}/.config/gtk-3.0/settings.ini
-path_localbin=/usr/local/bin
-path_history=${home}/.bash_history
+PATHWAY_OPT='/opt'
+PATHWAY_FONTS="$home/Documents/fonts"
+PATHWAY_ICONS="$home/.icons"
+PATHWAY_THEMES="$home/.themes"
+PATHWAY_TERMINATOR="$home/.config/terminator/config"
+PATHWAY_TREE="$home/others"
+PATHWAY_GTK="$home/.config/gtk-3.0/settings.ini"
+PATHWAY_LOCALBIN="/usr/local/bin"
+PATHWAY_HISTORY="$home/.bash_history"
 
 # ls commands to save.
-ls -1 ${path_opt} | cat -n | tr -s ' ' >${array_pathway_backup[opt]}/opt_programs.txt
-ls -1 ${path_fonts} | cat -n | tr -s ' ' >${array_pathway_backup[fonts]}/fonts.txt
-ls -1 ${path_icons} | cat -n | tr -s ' ' >${array_pathway_backup[iconthemes]}/icons.txt
-ls -1 ${path_themes} | cat -n | tr -s ' ' >${array_pathway_backup[iconthemes]}/themes.txt
-ls -1 ${path_localbin} | cat -n | tr -s ' ' >${array_pathway_backup[localbin]}/binaries.txt
+ls -1 "$PATHWAY_OPT" | cat -n | tr -s ' ' >"${ARRAY_PATHWAY_BACKUP['opt']}/opt_programs.txt"
+ls -1 "$PATHWAY_FONTS" | cat -n | tr -s ' ' >"${ARRAY_PATHWAY_BACKUP['fonts']}/fonts.txt"
+ls -1 "$PATHWAY_ICONS" | cat -n | tr -s ' ' >"${ARRAY_PATHWAY_BACKUP['iconthemes']}/icons.txt"
+ls -1 "$PATHWAY_THEMES" | cat -n | tr -s ' ' >"${ARRAY_PATHWAY_BACKUP['iconthemes']}/themes.txt"
+ls -1 "$PATHWAY_LOCALBIN" | cat -n | tr -s ' ' >"${ARRAY_PATHWAY_BACKUP['localbin']}/binaries.txt"
 
 # cp commands to save.
-cp -f ${path_terminator} ${array_pathway_backup[terminator]}/config.txt
-cp -f ${path_gtk} ${array_pathway_backup[gtk]}/settings.txt
+cp -f "$PATHWAY_TERMINATOR" "${ARRAY_PATHWAY_BACKUP['terminator']}/config.txt"
+cp -f "$PATHWAY_GTK" "${ARRAY_PATHWAY_BACKUP['gtk']}/settings.txt"
 
 # others commands to save.
-tree ${path_tree} >${array_pathway_backup[others]}/tree_output.txt
-dpkg -l >${array_pathway_backup[dpkg]}/list.txt
-neofetch >${array_pathway_backup[neofetch]}/infos.txt
-gzip -cv9 "$path_history" > "${array_pathway_backup[history]}"/`date +%y-%m-%d_%H%M%S_bash-history.gz`
+tree "$PATHWAY_TREE" >"${ARRAY_PATHWAY_BACKUP['others']}/tree_output.txt"
+dpkg -l >"${ARRAY_PATHWAY_BACKUP['dpkg']}/list.txt"
+neofetch >"${ARRAY_PATHWAY_BACKUP['neofetch']}/infos.txt"
+
+# complex commands to save.
+{
+	FILE_NAME_HISTORY="${ARRAY_PATHWAY_BACKUP['history']}/`date +%y-%m-%d_%H%M%S_bash-history.gz`"
+	gzip -cv9 "$PATHWAY_HISTORY" > "$FILE_NAME_HISTORY"
+	chmod 600 "$FILE_NAME_HISTORY"
+}
 
 # clean ups.
 cleanup-history
