2f70bd92175bad9b69b84d61550df923f95472e4
rhuanpk <developer@rhuanpk.net>
2023-10-20 08:22:05 -0300

    update: agora é aceito aliases (e também funções) passando BASH_ENV no inicio do comando.

--- scripts/repeat.sh
+++ scripts/repeat.sh
@@ -2,21 +2,21 @@
 
 # Loop for commands.
 
-# >>> variable declarations !
+# >>> variable declarations!
+script=`basename $(readlink -f "$0")`
 
-script=$(basename "${0}")
-home=${HOME:-/home/${USER:-$(whoami)}}
+#home=${HOME:-/home/${USER:-`id -nu`}}
+#home=${HOME:-/home/${USER:-`whoami`}}
 
-# >>> function declarations !
-
-verify_privileges() {
+# >>> function declarations!
+verify-privileges() {
 	[ $UID -eq 0 ] && {
 		echo -e "ERROR: Run this program without privileges!\nExiting..."
 		exit 1
 	}
 }
 
-print_usage() {
+print-usage() {
         cat <<- EOF
 		#######################################################################################
 		#
@@ -27,9 +27,9 @@ print_usage() {
 		#
 		# Parâmetros passados:
 		#
-		#    1: Tempo (em segundos) para atualização do comando; # Proteção de aspas opcional.
+		# 	1: Tempo (em segundos) para atualização do comando; # Proteção de aspas opcional.
 		#
-		#    2: Comando a ser repetido; # Proteção de aspas opcional.
+		# 	2: Comando a ser repetido; # Proteção de aspas opcional.
 		#
 		# Exemplos:
 		#
@@ -38,6 +38,10 @@ print_usage() {
 		# $script 1 'sudo fdisk -l /dev/sda'
 		#
 		# $script 5 "ls -lhF ~/*"
+		#
+		# Dicas:
+		#
+		# 	1. Caso deseje repitira algum alias ou função customizade passe com BASH_ENV.
 		#
 		#######################################################################################
 	EOF
@@ -48,20 +52,24 @@ print_usage() {
 
 set +o histexpand
 
-#verify_privileges
+#verify-privileges
 [ "${1,,}" = '-h' -o "${1,,}" = '--help' ] && {
-        print_usage
+        print-usage
         exit 1
 }
 
 # >>> *** PROGRAM START *** !
-unset time command
+unset TIME COMMAND
 
-time=${@:1:1}
-command="${*:2}"
+TIME=${@:1:1}
+COMMAND=${*:2}
+
+if alias "$COMMAND" &>/dev/null; then
+	COMMAND=$(sed -En "s/^alias $COMMAND='(.*)'/\1/p" <<< "`alias $COMMAND`")
+fi
 
 while :; do
-	sleep ${time:?'needs informe a time delay as first param!'}
+	sleep ${TIME:?'needs informe a time delay as first param!'}
 	clear
-	eval "${command:?'needs informe a command to run as second param!'}"
+	eval "${COMMAND:?'needs informe a command to run as second param!'}"
 done
