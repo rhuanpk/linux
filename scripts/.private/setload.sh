dd3fe610109f79a9841571437d89fce9bec483bc
rhuanpk <developer@rhuanpk.net>
2023-11-01 00:52:37 -0300

    update: refactor script

--- scripts/.private/setload.sh
+++ scripts/.private/setload.sh
@@ -1,25 +1,73 @@
 #!/usr/bin/env bash
-sudo='sudo'
+
+# This script sets up global variables for the repositories.
+# Has defined the variables passed as param to the setload function in /etc/environment file.
+
+# >>> built-in sets!
+#set -e
+
+# >>> variable declaration!
+script="`basename $(readlink -f "$0")`"
+uid="${UID:-`id -u`}"
+user="${USER:-`id -un "$uid"`}"
+home="/home/$user"
+
+SUDO='sudo'
+
+# >>> function declaration!
+usage() {
+cat << EOF
+Usage: ./$script [<option>]
+
+Options:
+	-s: Forces keep sudo;
+	-r: Forces unset sudo;
+	-h: Print this help.
+EOF
+}
+
 setload() {
-	variable_name=${1:?'need a variable to set!'}
-	repo_name=${2:?'needs the name of the repository to be loaded!'}
-	home=/home/$(id -nu 1000)
-        environment_path=/etc/environment
-	full_path=$(
-		folders='pCloudDrive|googleDrive'
-		find ${home}/ -type f -name ".way-flag-${repo_name}.cfg" 2>&- \
-		| sed -E "/${folders:-$(uuidgen)}/d" \
+	VARIABLE_NAME=${1:?'needs a variable to set'}
+	REPO_NAME=${2:?'needs the name of the repository to be loaded'}
+
+        ENVIRONMENT_PATH=/etc/environment
+	FULL_PATH=$(
+		EXCLUDE_FOLDERS='pCloudDrive|googleDrive|tmp'
+		find $home/ -type f -name ".way-flag-$REPO_NAME.cfg" 2>&- \
+		| sed -E "/${EXCLUDE_FOLDERS:-`uuidgen`}/d" \
 		| xargs dirname 2>&- \
 		| tail -1
 	)
-	line_and_path=$(grep -nE "${variable_name}=.*$" ${environment_path:?'whats the environment path?'})
-	[ ! -z "${line_and_path}" ] && {
-		if ! $sudo sed -i "${line_and_path%:*}s~=${line_and_path#*=}~=${full_path}~" ${environment_path}; then
-			echo $full_path
+	LINE_AND_PATH=$(grep -nE "$VARIABLE_NAME=.*$" ${ENVIRONMENT_PATH:?'missing environment path'})
+	[ ! -z "$LINE_AND_PATH" ] && {
+		# this command grab the return code of command not found case sudo is not found what cause no errexit triggered by `set -e`
+		if ! $SUDO sed -i "${LINE_AND_PATH%:*}s~=${LINE_AND_PATH#*=}~=$FULL_PATH~" $ENVIRONMENT_PATH 2>&-; then
+			echo $FULL_PATH
 		fi
-	} || echo $full_path
+	} || echo $FULL_PATH
 }
-[ '-w' = "$1" ] && { unset sudo; shift; }
+
+
+# >>> pre statements!
+while getopts 'srh' OPTION; do
+	case "$OPTION" in
+		s) FLAG_SUDO=true;;
+		r) FLAG_ROOT=true;;
+		:|?|h) usage; exit 2;;
+	esac
+done
+shift $(("$OPTIND"-1))
+
+if [[ -z "$SUDO" && "$uid" -ne 0 ]]; then
+	echo "$script: run with root privileges"
+	exit 1
+elif ! "${FLAG_SUDO:-false}"; then
+	if "${FLAG_ROOT:-false}" || [[ -n "$SUDO" && "$uid" -eq 0 ]]; then
+		unset SUDO
+	fi
+fi
+
+# ***** PROGRAM START *****
 setload PK_LOAD_CFGBKP cfg-bkp
 setload PK_LOAD_LINUX linux
 #setload PK_LOAD_PKUTILS pkutils
