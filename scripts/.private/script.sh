58db18d126e81512131e4373630f3090c86cf08a
rhuanpk <developer@rhuanpk.net>
2023-11-01 00:27:54 -0300

    update: improve script tamplate logic

--- scripts/.private/script.sh
+++ scripts/.private/script.sh
@@ -7,26 +7,40 @@
 
 # >>> variable declaration!
 script="`basename $(readlink -f "$0")`"
-user="${USER:-`id -un`}"
+uid="${UID:-`id -u`}"
+user="${USER:-`id -un "$uid"`}"
 home="/home/$user"
 
 SUDO='sudo'
 
 # >>> function declaration!
 usage() {
-	echo "Usage; ./$script"
+	echo "usage; ./$script"
 }
 
 # >>> pre statements!
-[ "${UID:-`id -u`}" -eq 0 ] && { unset SUDO; }
-
-while getopts 'h' OPTION; do
+while getopts 'srh' OPTION; do
 	case "$OPTION" in
+		s) FLAG_SUDO=true;;
+		r) FLAG_ROOT=true;;
 		:|?|h) usage; exit 2;;
 	esac
 done
 shift $(("$OPTIND"-1))
 
+# Keeping set up the `SUDO` variable makes automatically retain or discard the sudo command.
+# `-s` option forces to keep the sudo command.
+# `-r` option forces to "unset" sudo command.
+# Comment the `SUDO` variable to require run as root.
+# Case not use sudo in the script don't worry, nothing changes.
+if [[ -z "$SUDO" && "$uid" -ne 0 ]]; then
+	echo "$script: run with root privileges"
+	exit 1
+elif ! "${FLAG_SUDO:-false}"; then
+	if "${FLAG_ROOT:-false}" || [[ -n "$SUDO" && "$uid" -eq 0 ]]; then
+		unset SUDO
+	fi
+fi
 
 # ***** PROGRAM START *****
 # <commands>
