282a6ba7f600a6f7cb9267d9e6131c4d38bc3f4a
rhuanpk <developer@rhuanpk.net>
2023-11-16 11:48:53 -0300

    update: template script

--- scripts/.private/script.sh
+++ scripts/.private/script.sh
@@ -7,6 +7,7 @@
 
 # >>> variable declaration!
 readonly version='0.0.0'
+location="`realpath "$0"`"
 script="`basename "$0"`"
 uid="${UID:-`id -u`}"
 user="`id -un "${uid/#0/1000}"`"
@@ -31,30 +32,34 @@ Options:
 EOF
 }
 
-# >>> pre statements!
-while getopts 'srvh' OPTION; do
-	case "$OPTION" in
-		s) FLAG_SUDO=true;;
-		r) FLAG_ROOT=true;;
-		v) echo "$version"; exit 0;;
-		:|?|h) usage; exit 2;;
-	esac
-done
-shift $(("$OPTIND"-1))
-
 # Keeping set up the `SUDO` variable makes automatically retain or discard the sudo command.
 # `-s` option forces to keep the sudo command.
 # `-r` option forces to "unset" sudo command.
 # Comment the `SUDO` variable to require run as root.
 # Case not use sudo in the script don't worry, nothing changes.
-if [[ -z "$SUDO" && "$uid" -ne 0 ]]; then
-	echo "$script: run with root privileges"
-	exit 1
-elif ! "${FLAG_SUDO:-false}"; then
-	if "${FLAG_ROOT:-false}" || [ "$uid" -eq 0 ]; then
-		unset SUDO
+privileges() {
+	FLAG_SUDO="${1:?needs sudo flag}"
+	FLAG_ROOT="${2:?needs root flag}"
+	if [[ -z "$SUDO" && "$uid" -ne 0 ]]; then
+		echo "$script: run with root privileges"
+		exit 1
+	elif ! "$FLAG_SUDO"; then
+		if "$FLAG_ROOT" || [ "$uid" -eq 0 ]; then
+			unset SUDO
+		fi
 	fi
-fi
+}
+
+# >>> pre statements!
+while getopts 'srvh' option; do
+	case "$option" in
+		s) privileges true false;;
+		r) privileges false true;;
+		v) echo "$version"; exit 0;;
+		:|?|h) usage; exit 2;;
+	esac
+done
+shift $(("$OPTIND"-1))
 
 # ***** PROGRAM START *****
 # <commands>
