2bfc4d366c253190afdf17764f7ea3d711b432a3
rhuanpk <developer@rhuanpk.net>
2023-11-26 11:17:22 -0300

    wip

--- scripts/volume-encryption-utility.sh
+++ scripts/volume-encryption-utility.sh
@@ -1,138 +1,152 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
-# Volume Encryption Utility (veu).
+# Volume Encryption Utility (VEU).
 
-# >>> variable declarations !
-
-script=$(basename "${0}")
-home=${HOME:-/home/${USER:-$(whoami)}}
-
-# >>> function declarations !
-
-verify_privileges() {
-	[ $UID -eq 0 ] && {
-		echo -e "ERROR: Run this program without privileges!\nExiting..."
-		exit 1
-	}
-}
-
-print_usage() {
-	cat <<- eof
-		###########################################################################
-		#
-		# >>> $script !
-		#
-		# Its a volume encryption utility (a bash script with simple instructions).
-		#
-		# OPTIONS:
-		#
-		# 	-h: print usage and exit.
-		# 	-s: print status of volume.
-		# 	-p </path/to/folder>: set as new folder to used like the volume.
-		# 	-m: mount the volume.
-		# 	-u: umount the volume.
-		#
-		###########################################################################
-	eof
-}
-
-# >>> pre statements !
-
+# >>> built-in sets!
 set +o histexpand
 
-verify_privileges
-[ ${#} -lt 1 ] && {
-        print_usage
-        exit 1
+# >>> variable declaration!
+readonly version='1.2.0'
+location="`realpath "$0"`"
+script="`basename "$0"`"
+uid="${UID:-`id -u`}"
+
+SUDO='sudo'
+VOLUME_PATH='/tmp/private'
+#array()
+#options=`IFS=,; echo "${array[*]}"`
+
+# >>> function declaration!
+usage() {
+cat << EOF
+$script v$version
+
+Its a volume encryption utility (a bash script with simple instructions).
+
+Usage: $script [<options>]
+
+Options:
+	-t: Print status of volume;
+	-p </path/to/folder>: Set as new folder to used like the volume;
+	-m: Mount the volume;
+	-u: Umount the volume;
+	-s: Forces keep sudo;
+	-r: Forces unset sudo;
+	-v: Print version;
+	-h: Print this help.
+EOF
 }
 
-# >>> *** PROGRAM START *** !
-volume_path=/tmp/tmp/private
-
-is_mounted_status=$(
-	if mountpoint ${volume_path}/ &>/dev/null; then
-		echo true
-	else
-		echo false
-	fi
-)
-
-print_status() {
+print-status() {
 	cat <<- eof
-		current folder set: $volume_path/
-		is-mounted: $is_mounted_status
+		Current folder setuped: "$VOLUME_PATH/"
+		Is-mounted: `if is-mounted; then echo true; else echo false; fi`
 	eof
 }
 
-change_path() {
-	new_path=${1:?'need a path to set new mount point!'}
-	script_path=$(which ${script})
-	line_and_path=$(grep -nE '^(volume_path=).*$' ${script_path:?'path of script not set!'})
-	if ! sudo sed -i "${line_and_path%:*}s~${line_and_path#*=}~${new_path}~" ${script_path}; then
-		echo 'Failed on changing!'
+privileges() {
+	FLAG_SUDO="${1:?needs sudo flag}"
+	FLAG_ROOT="${2:?needs root flag}"
+	if [[ -z "$SUDO" && "$uid" -ne 0 ]]; then
+		echo "$script: run with root privileges"
 		exit 1
-	else
-		echo 'Successful on changing!'
-		exit 0
-	fi
-}
-
-mount_private() {
-	try_mount() {
-		y_command=$1
-		if ! error_message=$(eval "${y_command}" 3>&1 1>&2 2>&3); then
-			cat <<- eof
-				Failed on mounting! ${error_message:+$(echo -e "\nerror message: ${error_message}")}
-			eof
-			exit 1
-		else
-			echo 'Successful on mounting!'
-			exit 0
+	elif ! "$FLAG_SUDO"; then
+		if "$FLAG_ROOT" || [ "$uid" -eq 0 ]; then
+			unset SUDO
 		fi
-	}
-	if $is_mounted_status; then
-		echo "${volume_path} already mounted!"
-		exit 1
 	fi
-	[ ! -d ${volume_path}/ ] && {
-		try_mount 'mkdir -p ${volume_path}/ && chmod 0600 ${volume_path}/ && sudo mount -t ecryptfs ${volume_path}/ ${volume_path}/'
+}
+
+default-message() {
+	RETURN="${1:?needs a code to verify}"
+	OPERATION="${2:?needs a message to show}"
+	OUTPUT="$3"
+	[ "$RETURN" -eq 0 ] && {
+		echo "$script: success on $OPERATION"
 	} || {
-		try_mount 'sudo mount -t ecryptfs ${volume_path}/ ${volume_path}/ -o key=passphrase,ecryptfs_cipher=aes,ecryptfs_key_bytes=32,ecryptfs_passthrough=n'
+		echo -e "$script: failed on $OPERATION:\n$OUTPUT"
 	}
 }
 
-umount_private() {
-	if ! $is_mounted_status; then
-		echo "${volume_path} already unmounted!"
-		exit 1
-	fi
-	if ! error_message=$(sudo umount $volume_path); then
-		cat <<- eof
-			Failed on mounting! ${error_message:+$(echo -e "\nerror message: ${error_message}")}
-		eof
-		exit 1
-	else
-		echo 'Successful on umounting!'
+check-needs() {
+	PACKAGES=('ecryptfs-utils')
+	for package in "${PACKAGES[@]}"; do
+		if ! dpkg -s "$package" &>/dev/null; then
+			read -p "$script: this script needs the \"$package\" package but not installed, install this? [Y/n] " answer
+			[ -z "$answer" ] || [ 'y' = "${answer,,}" ] && $SUDO apt install -y "$package"
+		fi
+	done
+}
+
+change-path() {
+	NEW_PATH="${1:?need a path to set new mount point}"
+	LINE_AND_PATH=`grep -nE '^VOLUME_PATH=.*$' "$location"`
+	OUTPUT=$($SUDO sed -i "${LINE_AND_PATH%:*}s~${LINE_AND_PATH#*=}~'`realpath ${NEW_PATH/%\//}`'~" "$location" 2>&1)
+	RETURN="$?"
+	default-message "$RETURN" 'changing path of private folder' "$OUTPUT"
+	exit "$RETURN"
+}
+
+mount-private() {
+	if is-mounted; then
+		echo "$script: \"$VOLUME_PATH/\" already mounted"
 		exit 0
 	fi
+	[ ! -d "$VOLUME_PATH/" ] && {
+		OUTPUT=$(
+			mkdir -p "$VOLUME_PATH/" \
+			&& chmod 600 "$VOLUME_PATH/" \
+			&& $SUDO mount \
+				--types ecryptfs \
+				"$VOLUME_PATH/" "$VOLUME_PATH/" \
+			3>&1 1>&2 2>&3
+		)
+	} || {
+		# criar cada variável e fazer outra função para passar de forma interativa perguntando o valor de cada variável (algumas com padrão (opção -i do read))
+		# não mudar opção padrão do FNEK a menos que saiba o que está fazendo (colocar isso no usage)
+		# ecryptfs_enable_filename_crypto=yes
+		OUTPUT=$(
+			$SUDO mount \
+				--types ecryptfs \
+				--options \
+					key=passphrase,ecryptfs_cipher=aes,ecryptfs_key_bytes=32,ecryptfs_passthrough=no \
+				"$VOLUME_PATH/" "$VOLUME_PATH/" \
+			3>&1 1>&2 2>&3
+		)
+	}
+	RETURN="$?"
+	default-message "$RETURN" 'mounting private folder' "$OUTPUT"
+	exit "$RETURN"
 }
 
-check_needs() {
-	y_package=ecryptfs-utils
-	if ! dpkg -s ${y_package} &>/dev/null; then
-		read -rp "this script needs the ${y_package} package but not installed, install this? [Y/n] " answer
-		[ -z $answer ] || [ 'y' = ${answer,,} ] && sudo apt install $y_package -y
+umount-private() {
+	if ! is-mounted; then
+		echo "$script: \"$VOLUME_PATH/\" already unmounted"
+		exit 0
 	fi
+	OUTPUT=`$SUDO umount "$VOLUME_PATH/" 2>&1`
+	RETURN="$?"
+	default-message "$RETURN" 'unmounting private folder' "$OUTPUT"
+	exit "$RETURN"
 }
 
-check_needs
-while getopts 'hsp:mu' opt; do
-	case $opt in
-		h) print_usage; exit 0;;
-		s) print_status; exit 0;;
-		p) change_path ${OPTARG};;
-		m) mount_private;;
-		u) umount_private;;
-		?) print_usage; exit 1;;
+is-mounted() { mountpoint "$VOLUME_PATH/" &>/dev/null; }
+
+# >>> pre statements!
+check-needs
+
+while getopts 'tp:musrvh' option; do
+	case "$option" in
+		t) print-status; exit 0;;
+		p) change-path "$OPTARG";;
+		m) mount-private;;
+		u) umount-private;;
+		s) privileges true false;;
+		r) privileges false true;;
+		v) echo "$version"; exit 0;;
+		:|?|h) usage; exit 2;;
 	esac
 done
+shift $(("$OPTIND"-1))
+
+# ***** PROGRAM START *****
