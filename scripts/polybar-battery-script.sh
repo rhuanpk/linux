38959712595a0263e8f39c4044dfebbfab23b681
rhuanpk <developer@rhuanpk.net>
2024-01-08 20:21:21 -0300

    add: polybar vpn module

--- scripts/polybar-battery-script.sh
+++ scripts/polybar-battery-script.sh
@@ -37,5 +37,5 @@ shift $(("$OPTIND"-1))
 
 if ! "${IS_POWER_SUPPLY:-false}"; then
 	BATTERY_PERCENT=`acpi | tr -d '[[:blank:]]' | cut -d ',' -f 2`
-	echo "| Battery: ${BATTERY_PERCENT:-0%}"
+	echo "Battery: ${BATTERY_PERCENT:-0%} |"
 fi
---
+++ scripts/polybar-vpn-script.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/bash
+
+# Print the CPU usage for polybar module.
+
+# >>> variable declaration!
+readonly version='1.0.0'
+script="`basename "$0"`"
+
+# >>> function declaration!
+usage() {
+cat << EOF
+$script v$version
+
+Short description of how it works.
+
+Usage: $script [<options>]
+
+Options:
+	-v: Print version;
+	-h: Print this help.
+EOF
+}
+
+# >>> pre statements!
+while getopts 'vh' option; do
+	case "$option" in
+		v) echo "$version"; exit 0;;
+		:|?|h) usage; exit 2;;
+	esac
+done
+shift $(("$OPTIND"-1))
+
+# ***** PROGRAM START *****
+VALIDATES="tun0|wg0|`hostname`"
+[[ "`ip -br link | cut -d ' ' -f 1`" =~ ($VALIDATES) ]] && echo '(VPN) |' || echo '|'
