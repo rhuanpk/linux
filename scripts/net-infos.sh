25be20266555dfb202011d7a1b670641e3ce9c58
rhuanpk <developer@rhuanpk.net>
2024-02-13 15:28:02 -0300

    add: net-infos script

---
+++ scripts/net-infos.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/bash
+
+# Print network informations.
+
+# >>> variables declaration!
+readonly version='1.0.0'
+readonly script="`basename "$0"`"
+readonly uid="${UID:-`id -u`}"
+
+SUDO='sudo'
+
+# >>> functions declaration!
+usage() {
+cat << EOF
+$script v$version
+
+Print network informations.
+
+Usage: $script [<options>]
+
+Options:
+	-v: Print version;
+	-h: Print this help.
+EOF
+}
+
+privileges() {
+	FLAG_SUDO="${1:?needs sudo flag}"
+	FLAG_ROOT="${2:?needs root flag}"
+	if [[ -z "$SUDO" && "$uid" -ne 0 ]]; then
+		echo "$script: error: run with root privileges"
+		exit 1
+	elif ! "$FLAG_SUDO"; then
+		if "$FLAG_ROOT" || [ "$uid" -eq 0 ]; then
+			unset SUDO
+		fi
+	fi
+}
+
+check-needs() {
+	privileges false false
+	PACKAGES=('network-manager')
+	for package in "${PACKAGES[@]}"; do
+		if ! dpkg -s "$package" &>/dev/null; then
+			read -p "$script: info: is needed the \"$package\" package, install? [Y/n] " answer
+			[ -z "$answer" ] || [ 'y' = "${answer,,}" ] && $SUDO apt install -y "$package"
+		fi
+	done
+}
+
+# >>> pre statements!
+check-needs
+
+while getopts 'vh' option; do
+	case "$option" in
+		v) echo "$version"; exit 0;;
+		:|?|h) usage; exit 2;;
+	esac
+done
+shift $(("$OPTIND"-1))
+
+# ***** PROGRAM START *****
+SSID="`nmcli -g 'NAME' connection show --active | sed -n '1p'`"
+BSSID="`nmcli -g 802-11-wireless.seen-bssids connection show "$SSID" | tr -d '\\'`"
+IFNAME="`nmcli -g 'GENERAL.TYPE,GENERAL.DEVICE' device show | grep -A1 '^wifi$' | sed -n '2p'`"
+
+while :; do
+	clear
+
+	echo '> ip -br -c a'
+	ip -br -c a
+
+	echo -e "\n> nmcli connection show --active"
+	nmcli connection show --active
+
+	echo -e "\n> nmcli device wifi list bssid \"$BSSID\" ifname \"$IFNAME\""
+	nmcli device wifi list bssid "$BSSID" ifname "$IFNAME"
+
+	tput cup `tput cols` 0
+	sleep 3
+done
