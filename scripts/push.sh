26bb6abc49cfed6292167256b7c932e291054241
rhuanpk <rhuan.patriky@rhuanpk.net>
2024-04-18 20:42:39 -0300

    feat: improve push script

--- scripts/push.sh
+++ scripts/push.sh
@@ -3,7 +3,7 @@
 # It does the entire push process automatically and if no parameter is passed, it commits with a standard message.
 
 # >>> variables declaration!
-readonly version='2.1.0'
+readonly version='2.2.0'
 readonly script="`basename "$0"`"
 
 # >>> functions declaration!
@@ -16,24 +16,33 @@ If \`<message>\` param is not passed use 'refresh' message as default.
 
 For works fine, setup upstream linked branch with: \`git branch --set-upstream-to=<remote/branch>\`
 
-Usage: $script [<options>] ['<message>']
+Usage: $script [<options>] [<message>]
 
 Options:
+	-n: When the push to a new branch;
 	-v: Print version;
 	-h: Print this help.
 EOF
 }
 
 # >>> pre statements!
-while getopts 'vh' OPTION; do
+while getopts 'nvh' OPTION; do
 	case "$OPTION" in
+		n) flag_branch=true;;
 		v) echo "$version"; exit 0;;
-		:|?|h) usage; exit 2;;
+		h) usage; exit 1;;
+		*) exit 2;;
 	esac
 done
 shift $(("$OPTIND"-1))
 
 # ***** PROGRAM START *****
+message="${1:-wip}"
+remote="$(git remote -v | grep '(push)$' | awk '{print $2}')"
 git add ./
-git commit -m "${1:-refresh}"
-git push
+git commit -m "$message"
+if ! "${flag_branch:-false}"; then
+	git push
+else
+	git push -u "$remote" HEAD
+fi
