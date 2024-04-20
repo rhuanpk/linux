94c4eaf0301ce29db78c323c56e7028fe42d3f40
rhuanpk <rhuan.patriky@rhuanpk.net>
2024-04-20 18:28:01 -0300

    update: push script -n option

--- scripts/push.sh
+++ scripts/push.sh
@@ -11,10 +11,12 @@ usage() {
 cat << EOF
 $script v$version
 
-Automatic run \`git add ./\` -> \`git commit -m "<message>"\` -> \`git push\`.
-If \`<message>\` param is not passed use 'refresh' message as default.
+Automatic run \`git add ./' -> \`git commit -m "<message>"' -> \`git push'.
+If \`<message>' param is not passed use 'refresh' message as default.
 
-For works fine, setup upstream linked branch with: \`git branch --set-upstream-to=<remote/branch>\`
+The \`-n' option will use the remote defined as "origin".
+
+For works fine, setup upstream linked branch with: \`git branch --set-upstream-to=<remote/branch>'
 
 Usage: $script [<options>] [<message>]
 
@@ -38,11 +40,11 @@ shift $(("$OPTIND"-1))
 
 # ***** PROGRAM START *****
 message="${1:-wip}"
-remote="$(git remote -v | grep '(push)$' | awk '{print $2}')"
+#remote="$(git remote -v | grep '(push)$' | awk '{print $2}')"
 git add ./
 git commit -m "$message"
 if ! "${flag_branch:-false}"; then
 	git push
 else
-	git push -u "$remote" HEAD
+	git push -u origin HEAD
 fi
