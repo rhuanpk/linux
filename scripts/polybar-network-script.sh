51559c8469f055c155367b5c81c1fc180f5e432b
rhuanpk <rhuan.patriky@rhuanpk.net>
2024-06-15 19:23:30 -0300

    refact: polybar network script

--- scripts/polybar-network-script.sh
+++ scripts/polybar-network-script.sh
@@ -3,7 +3,7 @@
 # Prints the local ip for polybar module.
 
 # >>> variables declaration!
-readonly version='1.0.0'
+readonly version='1.1.0'
 readonly script="`basename "$0"`"
 
 # >>> functions declaration!
@@ -31,23 +31,51 @@ done
 shift $(("$OPTIND"-1))
 
 # ***** PROGRAM START *****
-readonly TMP_FILE='/tmp/pns.sig'
-TMP_FILE_STATE="`[ -f "$TMP_FILE" ] && wc -l < "$TMP_FILE"`"
-INTERFACE="`ip a | grep -Em1 -A3 '[[:digit:]]+: (eth|enp|wl).*state UP'`"
-IP="`sed -nE 's/^.*inet (([[:digit:]]{1,3}.?){4})\/.*$/\1/p' <<< "$INTERFACE"`"
-[ -n "$INTERFACE" ] && {
-	if nmcli -g 'TYPE,STATE' device status | grep -qF 'ethernet:connected' || [[ "$(sed -nE 's/^[[:digit:]]+: ([[:alnum:]]+):.*$/\1/p' <<< "$INTERFACE")" =~ ^e ]]; then
-		TYPE='e_IP'
-	else
-		TYPE='w_IP'
-	fi
-} || TYPE='Network'
-[ -z "$IP" ] && IP='N/A'
-[ "${TMP_FILE_STATE:=0}" -eq 1 ] && {
-	IP+=" %{F#555555}-%{F-} `nmcli -t -f active,ssid device wifi | sed -nE 's/^yes:(.*)$/\1/p'`"
+# check if ifname is present
+check-ifname() {
+	local proc_file="${1:?need file to check}"
+	grep -qF "$ifname" "$proc_file"
 }
-[ "$TMP_FILE_STATE" -ge 2 ] && rm "$TMP_FILE"
-if pvs; then
-	IP+=' (%{F#FF00FF}VPN%{F-})'
+
+# set signal file
+readonly sig='/tmp/pns.sig'
+sig_state="`[ -f "$sig" ] && wc -l < "$sig"`"
+
+# get interface routes
+route="$(ip route | grep ^default | cut -d' ' -f'5,9,11' | sort -n -t' ' -k3 | head -1)"
+
+# set interface name
+ifname="$(cut -d' ' -f1 <<< "$route")"
+
+# set ip address
+ip="$(cut -d' ' -f2 <<< "$route")"
+: ${ip:=N/I}
+
+# set interface type
+if check-ifname /proc/net/wireless; then
+	iftype='w_IP'
+elif check-ifname /proc/net/dev; then
+	iftype='e_IP'
+else
+	iftype='Network'
 fi
-echo "%{F#10E3E3}$TYPE:%{F-} $IP"
+
+[ "${sig_state:=0}" -eq 1 ] && {
+	if [ "$iftype" = 'w_IP' ]; then
+		if which -s nmcli; then
+			ssid="$(nmcli -t -f name,device conn show --active | grep -F "$ifname" | cut -d':' -f1)"
+		#elif which -s iwgetid; then
+		#	ssid="$(iwgetid -r)"
+		#elif which -s iwconfig; then
+		#	ssid="$(iwconfig "$ifname" | sed -nE 's/^.*ESSID:"(.*)".*$/\1/p')"
+		fi
+		[ "$ssid" ] && ip+=" %{F#555555}-%{F-} %{F#FF00FF}$ifname:%{F-} $ssid"
+	elif [ "$iftype" = 'e_IP' ]; then
+		ip+=" %{F#555555}-%{F-} %{F#FF00FF}$ifname%{F-}"
+	fi
+}
+[ "$sig_state" -ge 2 ] && rm "$sig"
+if pvs; then
+	ip+=' (%{F#FF00FF}VPN%{F-})'
+fi
+echo "%{F#10E3E3}$iftype:%{F-} $ip"
