c6aa59acdeacefefed0d47116b2859be81eaa5ec
rhuanpk <rhuan.patriky@rhuanpk.net>
2024-06-28 12:33:26 -0300

    feat: improve to mount and umount automaticly

--- scripts/default-montage.sh
+++ scripts/default-montage.sh
@@ -3,7 +3,7 @@
 # Mount usual file system types with options of a file manger mount.
 
 # >>> variables declaration!
-readonly version='1.0.2'
+readonly version='1.1.0'
 readonly script="$(basename "$0")"
 readonly uid="${UID:-$(id -u)}"
 
@@ -54,13 +54,35 @@ done
 shift $(("$OPTIND"-1))
 
 # ***** PROGRAM START *****
+blkid-extract() {
+	local disk="${1:?$FUNCNAME: need a disk to extract infos}"
+	local field="${2:?$FUNCNAME: need a field to extract infos}"
+	$sudo blkid -o export "$disk" | sed -nE "s/^${field^^}=(.*)$/\1/p" | tr -d '\\'
+}
+#random() { mktemp -u "${RANDOM//?/X}"; }
 disk="${1:?need a disk to mount}"
-point="${2:?need a mount point to mount}"
-case "$($sudo blkid -o export "$disk" | sed -nE 's/^TYPE=(.*)$/\1/p')" in
-	ext4) options=',nosuid,nodev,errors=remount-ro,uhelper=udisks2';;
-	vfat) options=",nosuid,nodev,uid=`id -u`,gid=`id -g`,showexec,flush,uhelper=udisks2";;
-	exfat) options=",nosuid,nodev,uid=`id -u`,gid=`id -g`,uhelper=udisks2";;
-	ntfs) options=',nosuid,nodev,default_permissions,uhelper=udisks2';;
-	*) echo "$script: info: unusual filesystem type";;
-esac
-$sudo mount -o defaults$options "$disk" "$point"
+point="$(blkid-extract "$disk" 'label')"
+: ${point:=$(blkid-extract "$disk" 'partlabel')}
+: ${point:=$(blkid-extract "$disk" 'partuuid')}
+point="/mnt/$point"
+[ -d "$point" ] && {
+	already="$(df --output='source' "$point/" | sed -n 2p)"
+	[ "$(blkid-extract "$disk" 'partuuid')" != "$(blkid-extract "$already" 'partuuid')" ] && {
+		point+="-$(blkid-extract "$disk" 'uuid')"
+	}
+}
+if mountpoint -q "$point/"; then
+	$sudo umount -v "$point/"
+	$sudo rmdir -v "$point/"
+else
+	$sudo mkdir -pv "$point/"
+	case "$(blkid-extract "$disk" 'type')" in
+		ext4) options=',nosuid,nodev,errors=remount-ro,uhelper=udisks2';;
+		vfat) options=",nosuid,nodev,uid=`id -u`,gid=`id -g`,showexec,flush,uhelper=udisks2";;
+		exfat) options=",nosuid,nodev,uid=`id -u`,gid=`id -g`,uhelper=udisks2";;
+		ntfs) options=',nosuid,nodev,default_permissions,uhelper=udisks2';;
+		iso9660) options=",nosuid,nodev,uid=`id -u`,gid=`id -g`,uhelper=udisks2";;
+		*) echo "$script: info: unusual filesystem type";;
+	esac
+	$sudo mount -vo defaults$options "$disk" "$point/"
+fi
