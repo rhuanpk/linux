4e6f6736e7efbc67708278dfc967b94d1c87089a
rhuanpk <dev@rhuanpk.net>
2024-08-21 17:16:44 -0300

    refact: update base of script and add check-needs

--- scripts/sc2cb.sh
+++ scripts/sc2cb.sh
@@ -2,30 +2,83 @@
 
 # Send Color To ClipBoard sends the hex code of the color to your clipboard.
 
+# >>> built-in setups!
+set -eo pipefail
+
 # >>> variables declaration!
-readonly version='1.1.0'
-readonly script="`basename "$0"`"
+readonly version='1.2.0'
+readonly script="$(basename "$0")"
+readonly uid="${UID:-$(id -u)}"
 
 # >>> functions declaration!
 usage() {
 cat << EOF
 $script v$version
 
-Sends the hex code of the color to your clipboard.
+DESCRIPTION
+	Sends the hex code of the color to your clipboard.
 
-Usage: $script [<options>]
+USAGE
+	$script [<options>]
 
-Options:
-	-v: Print version;
-	-h: Print this help.
+OPTIONS
+	-s
+		Forces keep sudo.
+	-r
+		Forces unset sudo.
+	-v
+		Print version.
+	-h
+		Print this help.
 EOF
 }
 
+privileges() {
+	local flag_sudo="$1"
+	local flag_root="$2"
+	sudo='sudo'
+	if [[ -z "$sudo" && "$uid" -ne 0 ]]; then
+		echo "$script: error: run as root #sudo"
+		exit 1
+	elif ! "${flag_sudo:-false}"; then
+		if "${flag_root:-false}" || [ "$uid" -eq 0 ]; then
+			unset sudo
+		fi
+	fi
+}
+
+check-needs() {
+	privileges
+	local packages=('xclip')
+	for package in "${packages[@]}"; do
+		if ! dpkg -s "$package" &>/dev/null; then
+			echo -ne "$script: ask: needed \"$package\", "
+			read -rp  "install? [Y/n] "
+			[ -z "$REPLY" ] || [ 'y' = "${REPLY,,}" ] && {
+				$sudo apt install -y "$package"
+			}
+		fi
+	done
+	if ! which -s 'colorpicker'; then
+		local file_url='https://raw.githubusercontent.com/rhuanpk/misc/main/binaries/colorpicker'
+		local folder2save='/usr/local/bin'
+		local filename='colorpicker'
+		$sudo wget -P "$folder2save/" "$file_url"
+		$sudo chmod +x "$folder2save/$filename"
+	fi
+}
+
 # >>> pre statements!
-while getopts 'vh' option; do
+privileges
+check-needs
+
+while getopts 'srvh' option; do
 	case "$option" in
+		s) privileges true false;;
+		r) privileges false true;;
 		v) echo "$version"; exit 0;;
-		:|?|h) usage; exit 2;;
+		h) usage; exit 1;;
+		*) exit 2;;
 	esac
 done
 shift $(("$OPTIND"-1))
